7483027e95a7e52bb7947e625ba5484b8b8dcb7e
ryoji <ryoji@unko.com>
2023-08-19 17:18:10 +0900

    リクエストまで作成

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/winding")
+def get_winding():
+    current_latitude = request.args.get('current_latitude')
+    current_longitude = request.args.get('current_longitude')
+    destination = request.args.get('destination')
+
+    if current_latitude is None or current_longitude is None or destination is None:
+        error_message = "Both 'current_location' and 'destination_name' are required."
+        return jsonify({'error': error_message}), 400
+    return ['ok']
