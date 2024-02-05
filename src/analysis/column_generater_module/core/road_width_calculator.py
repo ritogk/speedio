13ea1ee4909f92858a96dd10f2a2ee4179dae562
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-02-05 09:46:29 +0900

    モジュールを結合

--- src/analysis/column_generater_module/core/road_width_calculator.py
+++ src/analysis/column_generater_module/core/road_width_calculator.py
@@ -9,39 +9,68 @@ from pyproj import Transformer
 from shapely.ops import nearest_points
 import glob
 from rtree import Rtree
+import pickle
+import os
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
+
 
 class RoadWidthCalculator:
     road_center_s: gpd.GeoSeries
     road_center_i: Rtree
     road_edge_s: gpd.GeoSeries
     road_edge_i: Rtree
+
     def __init__(self, center_path: str, edge_path: str):
         file_list = glob.glob(f"{center_path}/*.geojson")
-        print(file_list)
-        gdf_list = [gpd.read_file(file) for file in file_list]
-        road_center_df = gpd.GeoDataFrame(pd.concat(gdf_list, ignore_index=True)).to_crs("EPSG:6677")
+        path = f"{os.path.dirname(os.path.abspath(__file__))}/center_gdf"
+        # print(path)
+        if os.path.exists(path):
+            print("load center_gdf")
+            with open(path, "rb") as f:
+                road_center_df = pickle.load(f)
+        else:
+            print("create center_gdf")
+            gdf_list = [gpd.read_file(file) for file in file_list]
+            road_center_df = gpd.GeoDataFrame(
+                pd.concat(gdf_list, ignore_index=True)
+            ).to_crs("EPSG:6677")
+            # 保存する
+            with open(path, "wb") as f:
+                pickle.dump(road_center_df, f)
         self.road_center_s = road_center_df["geometry"]
         self.road_center_i = self.road_center_s.sindex
-        
-        # file_list = glob.glob(f"{edge_path}/*.xml")
-        file_list = glob.glob(f"{edge_path}/*.geojson")
-        gdf_list = [gpd.read_file(file) for file in file_list]
-        road_edge_df = gpd.GeoDataFrame(pd.concat(gdf_list, ignore_index=True)).to_crs("EPSG:6677")
+
+        file_list = glob.glob(f"{edge_path}/*.xml")
+        # print(file_list)
+        path = f"{os.path.dirname(os.path.abspath(__file__))}/edge_gdf"
+        if os.path.exists(path):
+            print("load edge_gdf")
+            with open(path, "rb") as f:
+                road_edge_df = pickle.load(f)
+        else:
+            print("create edge_gdf")
+            gdf_list = [gpd.read_file(file) for file in file_list]
+            road_edge_df = gpd.GeoDataFrame(
+                pd.concat(gdf_list, ignore_index=True)
+            ).to_crs("EPSG:6677")
+            # 保存する
+            with open(path, "wb") as f:
+                pickle.dump(road_edge_df, f)
         self.road_edge_s = road_edge_df["geometry"]
         self.road_edge_i = self.road_edge_s.sindex
+        print("end")
+
     def _to_latlon(self, x, y):
         tr = Transformer.from_proj(6677, 6668)
         lat, lon = tr.transform(x, y)
         return lat, lon
 
-
     def _to_xy(self, lat, lon):
         tr = Transformer.from_proj(6668, 6677)
         x, y = tr.transform(lat, lon)
         return x, y
-    
+
     def calculate(self, st_coord: Point, ed_coord: Point) -> int:
         time_st = time.time()
         st_x, st_y = self._to_xy(st_coord.x, ed_coord.y)
@@ -50,23 +79,26 @@ class RoadWidthCalculator:
 
         nearest_lines = []
         area = []
+        # ２座標に近い中央線を求める。
         for point in points:
             # 空間インデックスを使用して最も近いLiのindexをneStringを取得
             # 星以下でエラーがでる
             indexs = self.road_center_i.nearest(point, max_distance=100)
             # なぜか先頭にindex番号0が含まれているので消す。
             index = indexs[1:]
-            line = self.road_center_s.iloc[index[0][0]]
-            a =  nearest_points(line, point)
-            syototu_point = a[0]
-            area.append(syototu_point)
-
-        nearest_lines.append(LineString(area))
+            if index.size != 0:
+                line = self.road_center_s.iloc[index[0][0]]
+                a = nearest_points(line, point)
+                syototu_point = a[0]
+                area.append(syototu_point)
+                nearest_lines.append(LineString(area))
 
         syototu_lines = []
         normals = []
         edfes_length = []
+        # print(nearest_lines)
         for line in nearest_lines:
+            print(line)
             # lineの0と1の点を取り出す
             p1 = line.coords[0]
             p2 = line.coords[1]
@@ -99,9 +131,17 @@ class RoadWidthCalculator:
                     nearest_collision_point = collision_point
                     # print(nearest_distance)
                     # print(to_latlon(nearest_collision_point.y, nearest_collision_point.x))
-                    syototu_lines.append({'line_string': LineString([src, dst]), 'distance': nearest_distance * 2})
+                    syototu_lines.append(
+                        {
+                            "line_string": LineString([src, dst]),
+                            "distance": nearest_distance * 2,
+                        }
+                    )
             normals.append(LineString([p2, p_normal]))
         # print(syototu_lines)
-        print(f"time: {time.time() - time_st}")
+        # print(f"time: {time.time() - time_st}")
 
-        return syototu_lines[0]["distance"]
+        if not syototu_lines:
+            return 0
+        else:
+            return syototu_lines[0]["distance"]
