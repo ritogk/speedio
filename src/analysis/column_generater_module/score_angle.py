d1b75c5693294166fbf3b6678f42372f055efa72
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-02 18:12:27 +0900

    標高と角度を正規化するようにした

--- src/analysis/column_generater_module/score_angle.py
+++ src/analysis/column_generater_module/score_angle.py
@@ -3,5 +3,5 @@ from pandas import Series
 
 
 def generate(gdf: GeoDataFrame) -> Series:
-    series = gdf["score"] / gdf["score"].max()
+    series = gdf["angle_and_length_radio"] / gdf["angle_and_length_radio"].max()
     return series
--- src/analysis/column_generater_module/score_elevation.py
+++ src/analysis/column_generater_module/score_elevation.py
@@ -3,5 +3,5 @@ from pandas import Series
 
 
 def generate(gdf: GeoDataFrame) -> Series:
-    series = gdf["score"] / gdf["score"].max()
+    series = gdf["elevation_and_length_radio"] / gdf["elevation_and_length_radio"].max()
     return series
