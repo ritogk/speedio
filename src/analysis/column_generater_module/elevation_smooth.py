a4fcf43506ebce9134ae85cc14fddb6f9e1cc52b
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-03 21:39:42 +0900

    refactor

--- src/analysis/column_generater_module/elevation_smooth.py
+++ src/analysis/column_generater_module/elevation_smooth.py
@@ -9,8 +9,7 @@ def generate(gdf: GeoDataFrame) -> Series:
     window_size = 4
 
     def func(row):
-        elevations = row.elevations
-        series = pd.Series(elevations)
+        series = pd.Series(row.elevation)
         # 移動平均でサンプリング4で平滑化する。平準化の影響で3つのデータが削除される。
         smooth = series.rolling(window=window_size).mean().dropna().tolist()
         return smooth
