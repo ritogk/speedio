0d7166a5a9385af8fce6d92057996b14521d7806
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-03 21:00:46 +0900

    fix: 標高取得と標高変化量の計算を別にファイルにした

--- src/analysis/column_generater_module/elevation_deltas.py
+++ src/analysis/column_generater_module/elevation_deltas.py
@@ -1,27 +1,17 @@
 from geopandas import GeoDataFrame
 from pandas import Series
 from .core import elevetion_service
+import pandas as pd
 
 
 # 標高の変化量を取得する
-def generate(gdf: GeoDataFrame, tif_path: str) -> tuple[Series, Series]:
-    elevetion_service_ins = elevetion_service.ElevationService(tif_path)
-
+def generate(gdf: GeoDataFrame) -> tuple[Series, Series]:
     def func(row):
-        locations = list(row.geometry.coords)
-        elevations = []
+        elevations = row.elevations
         elevation_deltas = 0
         prev_elevation = None
-
         # ジオメトリーの座標から標高を取得し、標高の変化量も計算する
-        for location in locations:
-            elevation = elevetion_service_ins.get_elevation(location[1], location[0])
-            if elevation is None:
-                print("elevation is None")
-                continue
-
-            elevations.append({"elevation": elevation, "location": location})
-
+        for elevation in elevations:
             # 標高変化量の計算
             if prev_elevation is not None:
                 elevation_abs = abs(elevation - prev_elevation)
@@ -29,16 +19,14 @@ def generate(gdf: GeoDataFrame, tif_path: str) -> tuple[Series, Series]:
                 if elevation_abs <= 40:
                     elevation_deltas += elevation_abs
                 else:
-                    print(
-                        f"The change in elevation is over 40 meters. st: {location}, ed: {prev_location}"
-                    )
+                    print(f"The change in elevation is over 40 meters")
+                    # print(
+                    #     f"The change in elevation is over 40 meters. st: {location}, ed: {prev_location}"
+                    # )
             prev_elevation = elevation
-            prev_location = location
 
-        return elevation_deltas, elevations
+        return elevation_deltas
 
-    results = gdf.apply(func, axis=1)
-    series_deltas = results.apply(lambda x: x[0])
-    series_elevations = results.apply(lambda x: x[1])
+    series_deltas = gdf.apply(func, axis=1)
 
-    return series_deltas, series_elevations
+    return series_deltas
