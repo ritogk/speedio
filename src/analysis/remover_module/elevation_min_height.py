636ffdb858ddf8f1d02c82e4e6d31555e2a8e1bc
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-04 09:03:36 +0900

    refactor

--- src/analysis/remover_module/elevation_min_height.py
+++ src/analysis/remover_module/elevation_min_height.py
@@ -1,6 +1,6 @@
 from geopandas import GeoDataFrame
 
 
-# 逆方向のエッジを削除する
+# 標高の変化が少ないエッジを削除する
 def remove(gdf: GeoDataFrame) -> GeoDataFrame:
     return gdf[gdf["elavation_height_and_length_ratio"] > 0.02]
