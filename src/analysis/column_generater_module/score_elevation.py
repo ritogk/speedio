673786a8614925b5be635701b3fb96f97cbac731
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-07 22:02:56 +0900

    標高の評価を最適化

--- src/analysis/column_generater_module/score_elevation.py
+++ src/analysis/column_generater_module/score_elevation.py
@@ -3,9 +3,9 @@ from pandas import Series
 
 
 # 標高を評価する
-# 距離と標高の比率が0.08mを超える場合は1とする。
+# 距離と標高の比率が0.07mを超える場合は1とする。
 def generate(gdf: GeoDataFrame) -> Series:
-    series = gdf["elavation_height_and_length_ratio"] / 0.08
+    series = gdf["elavation_height_and_length_ratio"] / 0.07
     ## 1以上の値は1とする
     series = series.apply(lambda x: 1 if x > 1 else x)
     return series
