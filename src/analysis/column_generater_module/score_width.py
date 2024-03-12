398d31e53a3e95c4c79ee52c665f228d3cb775ef
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-13 06:13:50 +0900

    fix: 道幅の評価にlanesも含めるようにした

--- src/analysis/column_generater_module/score_width.py
+++ src/analysis/column_generater_module/score_width.py
@@ -3,15 +3,16 @@ from pandas import Series
 
 
 # 道幅を評価する
-# 道幅で信頼でき情報源がalpsmapしかないのでその情報をもとに評価する
+# 道幅で信頼でき情報源がlanesとalpsmapしかないのでその情報をもとに評価する
 def generate(gdf: GeoDataFrame) -> Series:
 
-    # is_alpsmapがtrueかつalpsmap_min_widthが5.5以上のエッジを1にしてそれ以外を0.8にする
     def func(row):
+        score = 0.6
+        if row["lanes"] == "2":
+            score += 0.2
         if row["is_alpsmap"] and row["alpsmap_min_width"] >= 5.5:
-            return 1
-        else:
-            return 0.8
+            score += 0.2
+        return score
 
     series = gdf.apply(func, axis=1)
     return series
