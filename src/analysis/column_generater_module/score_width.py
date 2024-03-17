4e5ebb244b722fd900af97d23747aeb0a4869184
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-17 11:00:44 +0900

    feat: 道幅の評価をかえた

--- src/analysis/column_generater_module/score_width.py
+++ src/analysis/column_generater_module/score_width.py
@@ -3,15 +3,15 @@ from pandas import Series
 
 
 # 道幅を評価する
-# 道幅で信頼でき情報源がlanesとalpsmapしかないのでその情報をもとに評価する
+# 道幅で信頼できる情報源がlanesとalpsmapしかないのでその情報をもとに評価する
 def generate(gdf: GeoDataFrame) -> Series:
 
     def func(row):
-        score = 0.6
+        score = 0.5
         if row["lanes"] == "2":
-            score += 0.2
+            score += 0.25
         if row["is_alpsmap"] and row["alpsmap_min_width"] >= 5.5:
-            score += 0.2
+            score += 0.25
         return score
 
     series = gdf.apply(func, axis=1)
