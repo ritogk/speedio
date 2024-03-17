eb7fd631f196c3f0687619250830af1259172fc1
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-17 19:40:20 +0900

    refactor: フロント側いよせたので削除

--- src/analysis/column_generater_module/score_normalization.py
+++
@@ -1,8 +0,0 @@
-from geopandas import GeoDataFrame
-from pandas import Series
-from .core import normalize
-
-
-def generate(gdf: GeoDataFrame) -> Series:
-    series = normalize.min_max(gdf["score"])
-    return series
