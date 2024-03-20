72288807ed5075b3828e0387141d664a6c8ab5a5
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-20 14:06:30 +0900

    refactor

--- src/analysis/column_generater_module/connection_node_cnt.py
+++ src/analysis/column_generater_module/connection_node_cnt.py
@@ -1,28 +1,16 @@
 from geopandas import GeoDataFrame
 from pandas import Series
 import osmnx as ox
-
+import networkx as nx
 from tqdm import tqdm
 
 
 # エッジ内の分岐数を取得する
-def generate(
-    gdf: GeoDataFrame, latitude_start, longitude_start, latitude_end, longitude_end
-) -> Series:
+def generate(gdf: GeoDataFrame, graph: nx.Graph) -> Series:
     ox.settings.use_cache = True
     ox.settings.log_console = False
 
-    # エッジに含まれるノードを取得する
-    graph_all = ox.graph_from_bbox(
-        north=max(latitude_start, latitude_end),
-        south=min(latitude_start, latitude_end),
-        east=max(longitude_start, longitude_end),
-        west=min(longitude_start, longitude_end),
-        network_type="drive",
-        simplify=True,
-        retain_all=True,
-    )
-    all_nodes = ox.graph_to_gdfs(graph_all, nodes=True, edges=False)
+    all_nodes = ox.graph_to_gdfs(graph, nodes=True, edges=False)
 
     def func(row):
         # ジオメトリーの座標と一致するノードを取得する
