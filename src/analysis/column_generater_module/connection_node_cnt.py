8de973bb9ab70b315f2b8c64d72486dbe0762e9f
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-03-24 18:26:50 +0900

    feat: 分岐数計算処理の高速化

--- src/analysis/column_generater_module/connection_node_cnt.py
+++ src/analysis/column_generater_module/connection_node_cnt.py
@@ -11,12 +11,19 @@ def generate(gdf: GeoDataFrame, graph: nx.Graph) -> Series:
     ox.settings.log_console = False
 
     all_nodes = ox.graph_to_gdfs(graph, nodes=True, edges=False)
+    sindex_nodes = all_nodes.sindex
 
     def func(row):
-        # ジオメトリーの座標と一致するノードを取得する
-        nodes = all_nodes[all_nodes.geometry.intersects(row.geometry)]
-        # 進行方向と逆方向のノードを除外して分岐数を計算する
-        result = nodes["street_count"].sum() - (len(nodes) * 2)
+        # ジオメトリーの境界ボックス内のノードのインデックスを取得
+        sindex_matche_indexs = list(sindex_nodes.intersection(row.geometry.bounds))
+        # インデックスをNodeに変換
+        sindex_matche_nodes = all_nodes.iloc[sindex_matche_indexs]
+
+        # 境界ボックス内のノードの中で実際に交差するノードを取得。boudary内のノードから絞り込むのでめっちゃはやい。
+        matche_nodes = sindex_matche_nodes[sindex_matche_nodes.intersects(row.geometry)]
+
+        # 進行方向と逆方向のノードを除外して分岐数を計算
+        result = matche_nodes["street_count"].sum() - (len(matche_nodes) * 2)
         return result
 
     tqdm.pandas()
