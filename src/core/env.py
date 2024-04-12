fd954a4fe5aff3923d1b8411179b23a136580d4d
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-04-13 03:30:41 +0900

    feat: 道幅計算制御の切り替えをgsiのみを対象にした

--- src/core/env.py
+++ src/core/env.py
@@ -8,9 +8,9 @@ load_dotenv()
 def getEnv():
     POINT_ST = os.getenv("POINT_ST")
     POINT_ED = os.getenv("POINT_ED")
-    CONSIDER_WIDTH = os.getenv("CONSIDER_WIDTH")
+    CONSIDER_GSI_WIDTH = os.getenv("CONSIDER_GSI_WIDTH")
     return {
         "POINT_ST": POINT_ST.replace(" ", "").split(","),
         "POINT_ED": POINT_ED.replace(" ", "").split(","),
-        "CONSIDER_WIDTH": True if CONSIDER_WIDTH == "1" else False,
+        "CONSIDER_GSI_WIDTH": True if CONSIDER_GSI_WIDTH == "1" else False,
     }
