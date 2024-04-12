85669e0fd2b323e0858383cedb35a69ba2ea851b
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-04-13 02:32:09 +0900

    fix: 道幅スコアの計算ロジックを修正

--- src/analysis/column_generater_module/score_width.py
+++ src/analysis/column_generater_module/score_width.py
@@ -1,17 +1,26 @@
 from geopandas import GeoDataFrame
 from pandas import Series
+from .eye_measured_width import RoadCondition
 
 
 # 道幅を評価する
 # 道幅で信頼できる情報源がlanesとalpsmapしかないのでその情報をもとに評価する
 def generate(gdf: GeoDataFrame) -> Series:
-
     def func(row):
-        score = 0.4
-        if row["lanes"] == "2":
-            score += 0.2
-        if row["is_alpsmap"] and row["alpsmap_min_width"] >= 5.5:
-            score += 0.4
+        eye_measured_width = row["eye_measured_width"]
+        score = 0
+        if eye_measured_width == RoadCondition.UNCONFIRMED.value:
+            if row["lanes"] == "2":
+                score += 0.5
+            if row["is_alpsmap"] and row["alpsmap_min_width"] >= 5.5:
+                score += 0.5
+        else:
+            if eye_measured_width == RoadCondition.COMFORTABLE.value:
+                score = 1
+            elif eye_measured_width == RoadCondition.PASSABLE.value:
+                score = 0.6
+            elif eye_measured_width == RoadCondition.UNPLEASANT.value:
+                score = 0
         return score
 
     series = gdf.apply(func, axis=1)
