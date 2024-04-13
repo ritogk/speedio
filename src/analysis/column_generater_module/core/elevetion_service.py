ad6acc1429177d5f5433896636cc639a9801a676
ritogk <homing0321r4cfw@yahoo.co.jp>
2024-04-13 22:21:41 +0900

    feat: tifのepsg変換処理作成

--- src/analysis/column_generater_module/core/elevetion_service.py
+++ src/analysis/column_generater_module/core/elevetion_service.py
@@ -1,9 +1,20 @@
-from osgeo import gdal
+from osgeo import gdal, osr
 
 
 class ElevationService:
     def __init__(self, tif_path):
         self.dataset = gdal.Open(tif_path, gdal.GA_ReadOnly)
+        proj = self.dataset.GetProjection()
+        sr = osr.SpatialReference(wkt=proj)
+        if sr.AutoIdentifyEPSG() != gdal.CE_None:
+            print("false")
+        # EPSGが4326でない場合は変換
+        epsg = sr.GetAttrValue("AUTHORITY", 1)
+        if epsg != "4326":
+            print(f"EPSG: {epsg} → EPSG: 4326")
+            warp_options = gdal.WarpOptions(format="GTiff", dstSRS="EPSG:4326")
+            gdal.Warp(tif_path, self.dataset, options=warp_options)
+            self.dataset = gdal.Open(tif_path, gdal.GA_ReadOnly)
 
     def get_elevation(self, lat: int, lon: int) -> int | None:
         if self.dataset is None:
