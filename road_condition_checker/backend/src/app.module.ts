06cce5afed411a4b7df232efac4d2a9e8d3f524a
Your Name <you@example.com>
2024-04-16 22:39:39 +0900

    feat: typeorm install

--- road_condition_checker/backend/src/app.module.ts
+++ road_condition_checker/backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {User} from './db/user.entity'
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'speedia',
+    password: 'P@ssw0rd',
+    database: 'speedia',
+    entities: [User],
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
