4baef9a64fd24ae8ddeb2e6daf8ce94849d1815c
Your Name <you@example.com>
2024-04-18 09:46:26 +0900

    feat: openapi.ymlの出力

--- road_condition_checker/backend/src/main.ts
+++ road_condition_checker/backend/src/main.ts
@@ -1,18 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as openapi from './openapi';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    // .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    // .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  await openapi.setup(app);
   await app.listen(3000);
 }
 bootstrap();
---
+++ road_condition_checker/backend/src/openapi.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import * as yaml from 'js-yaml';
+
+export const setup = async (app: INestApplication) => {
+  const documentBuilder = new DocumentBuilder()
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  // swagger-viewerの起動
+  SwaggerModule.setup('api', app, document);
+
+  // openapi.ymlの出力
+  const yamlDocument = yaml.dump(document, {
+    skipInvalid: true,
+    noRefs: true,
+  });
+  const yamlPath = path.join(__dirname, 'openapi.yml');
+  await fs.writeFile(yamlPath, yamlDocument);
+};
