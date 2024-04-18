c63f2b029ed6a8463f8148b404127a3781aad570
Your Name <you@example.com>
2024-04-18 23:00:39 +0900

    feat: api実装

--- road_condition_checker/backend/src/locations/locations.module.ts
+++ road_condition_checker/backend/src/locations/locations.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { LocationsController } from './locations.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Location } from './entities/location.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Location])],
   controllers: [LocationsController],
   providers: [LocationsService],
 })
