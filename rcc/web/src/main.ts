1684bc6602ddae91ea0fb600970fcd9b7b9e12f6
Your Name <you@example.com>
2024-04-19 18:28:26 +0900

    openapi-stack-client やりかけ

--- rcc/web/src/main.ts
+++ rcc/web/src/main.ts
@@ -3,6 +3,20 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import OpenAPIClientAxios from 'openapi-client-axios'
+// import type { Client as ApiClient } from '@/types/openapi'
+
+// // ここでlocalhost:5173/openapi.ymlを読み出している・・・いやいいのか。
+// const api = new OpenAPIClientAxios({
+//   definition: 'http://localhost:3000/public/openapi.yml'
+// })
+// api.init()
+
+// const test = async () => {
+//   const client = await api.getClient<ApiClient>()
+//   await client.LocationsController_findAll()
+// }
+// test()
 
 const app = createApp(App)
 
