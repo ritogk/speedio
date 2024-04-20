a54ede1ab6e82ae545d5215d2d8be8aa2f08c324
Your Name <you@example.com>
2024-04-20 12:03:22 +0900

    web: 複数回fetchしないようにした

--- rcc/web/src/main.ts
+++ rcc/web/src/main.ts
@@ -4,13 +4,13 @@ import router from './router'
 import OpenAPIClientAxios from 'openapi-client-axios'
 import type { Client as ApiClient } from '@/types/openapi'
 
-const apiPrefix = import.meta.env.VITE_API_PREFIX
-const api = new OpenAPIClientAxios({
-  definition: `/${apiPrefix}/public/openapi.yml`
-})
-api.init()
-
 const test = async () => {
+  const apiPrefix = import.meta.env.VITE_API_PREFIX
+  const api = new OpenAPIClientAxios({
+    definition: `/${apiPrefix}/public/openapi.yml`
+  })
+  // NOTE: initを実行するとなぜかopenapi.ymlを2回fetchしてしまう
+  // api.init()
   const client = await api.getClient<ApiClient>()
   client.defaults.baseURL = apiPrefix
   await client.LocationsController_findAll()
