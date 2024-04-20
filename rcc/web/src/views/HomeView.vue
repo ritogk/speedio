92ba25f7b235ee7f91c9bc4a1453cd5121e5e7ff
Turbobot <turbobot@vercel.com>
2024-04-20 17:12:36 +0900

    feat(create-turbo): apply official-starter transform

--- rcc/web/src/views/HomeView.vue
+++ rcc/web/src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div style="display: flex; width: 100%">
+    <div style="flex: 3; background-color: aqua">street_view_area</div>
+    <div style="flex: 1; background-color: blueviolet">google_map_area</div>
+    <div style="flex: 1; background-color: darkgreen">control-area</div>
   </div>
+  <div></div>
 </template>
