130d093f1108e2fb8c1b870d3f823973e8213552
Your Name <you@example.com>
2024-04-20 11:03:40 +0900

    feat: apiのプロキシ設定

--- rcc/web/src/main.ts
+++ rcc/web/src/main.ts
@@ -1,20 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import OpenAPIClientAxios from 'openapi-client-axios'
-// import type { Client as ApiClient } from '@/types/openapi'
+import OpenAPIClientAxios from 'openapi-client-axios'
+import type { Client as ApiClient } from '@/types/openapi'
 
-// // ここでlocalhost:5173/openapi.ymlを読み出している・・・いやいいのか。
-// const api = new OpenAPIClientAxios({
-//   definition: 'http://localhost:3000/public/openapi.yml'
-// })
-// api.init()
+const api = new OpenAPIClientAxios({
+  definition: '/api/public/openapi.yml'
+})
+api.init()
 
-// const test = async () => {
-//   const client = await api.getClient<ApiClient>()
-//   await client.LocationsController_findAll()
-// }
-// test()
+const test = async () => {
+  const client = await api.getClient<ApiClient>()
+  await client.LocationsController_findAll()
+}
+test()
 
 const app = createApp(App)
 
