7308114553651a502be78e95d4d4e28556da32a6
Your Name <you@example.com>
2024-04-20 10:35:56 +0900

    feat: vueのssl化

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # speedio
+
 運転が楽しいと感じるワインディングを抽出するスクリプト
 
 ## 抽出したワインディング
-![image](https://github.com/ritogk/speedio/assets/72111956/9c29dc11-b058-4f5f-8ffa-31d3857a792d)
 
+![image](https://github.com/ritogk/speedio/assets/72111956/9c29dc11-b058-4f5f-8ffa-31d3857a792d)
 
 ## setup
 
@@ -16,4 +17,12 @@ conda env create -f environment.yml
 
 ```
 conda activate touge-searcher
+```
+
+# rcc
+
+```
+cd rcc/web/.cert
+mkcer install
+mkcert localhost
 ```
--- rcc/web/vite.config.ts
+++ rcc/web/vite.config.ts
@@ -3,16 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    open: true, // 起動時にブラウザを開く
+    port: 5173,
+    https: {
+      key: fs.readFileSync('./cert/localhost-key.pem'),
+      cert: fs.readFileSync('./cert/localhost.pem')
     }
   }
 })
