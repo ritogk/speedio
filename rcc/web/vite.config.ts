6dcb8965872dd1763dfc85e7985526d552c94ea7
Your Name <you@example.com>
2024-04-20 11:48:34 +0900

    fix: api周りの環境変数を作成

--- rcc/web/vite.config.ts
+++ rcc/web/vite.config.ts
@@ -1,31 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import fs from 'fs'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    open: true, // 起動時にブラウザを開く
-    port: 5173,
-    https: {
-      key: fs.readFileSync('./.cert/localhost-key.pem'),
-      cert: fs.readFileSync('./.cert/localhost.pem')
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  const apiPath = process.env.VITE_API_PATH
+  const apiPrefix = '/' + process.env.VITE_API_PREFIX
+  return defineConfig({
+    plugins: [vue(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true, // サーバーのオリジンを変更する
-        rewrite: (path) => path.replace(/^\/api/, '') // プロキシ先のパスに書き換える
+    server: {
+      open: true, // 起動時にブラウザを開く
+      port: 5173,
+      https: {
+        key: fs.readFileSync('./.cert/localhost-key.pem'),
+        cert: fs.readFileSync('./.cert/localhost.pem')
+      },
+      proxy: {
+        [apiPrefix]: {
+          target: apiPath,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp(`^${apiPrefix}`), '') // プロキシ先のパスを動的に書き換える
+        }
       }
     }
-  }
-})
+  })
+}
