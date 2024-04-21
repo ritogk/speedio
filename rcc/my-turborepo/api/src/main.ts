de0ffea28a18a58e0cba565f4686ee3fe16868f5
Your Name <you@example.com>
2024-04-21 11:26:46 +0900

    monorepo構成に変更

--- rcc/my-turborepo/api/src/main.ts
+++ rcc/my-turborepo/api/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as openapi from './openapi';
+//import * as openapi from './generate-openapi';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await openapi.setup(app);
+  // await openapi.setup(app);
   await app.listen(3000);
 }
 bootstrap();
