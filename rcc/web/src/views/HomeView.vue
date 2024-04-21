e98d13ad8e200000836556bb6e637ba9852d8579
Your Name <you@example.com>
2024-04-21 19:12:52 +0900

    street_viewの読み込み作成

--- rcc/web/src/views/HomeView.vue
+++ rcc/web/src/views/HomeView.vue
@@ -19,11 +19,21 @@ onMounted(() => {
       zoom: 8
     })
   })
+  loader.importLibrary('streetView').then((google) => {
+    const panorama = new google.StreetViewPanorama(document.getElementById('pano') as HTMLElement, {
+      position: { lat: 35.334261616547465, lng: 136.99613190333835 },
+      pov: {
+        heading: 34,
+        pitch: 10
+      }
+    })
+  })
 })
 </script>
 
 <template>
   <div id="map" style="height: 400px">Loading map...</div>
+  <div id="pano" style="height: 400px">Loading map...</div>
   <div style="display: flex; width: 100%">
     <div style="flex: 3; background-color: aqua">street_view_area</div>
     <div style="flex: 1; background-color: blueviolet">google_map_area</div>
