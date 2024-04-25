d69fb891e9e466334c5506a3d5a378057258a02b
Your Name <you@example.com>
2024-04-26 08:59:23 +0900

    apiClientをグローバルに作絵師

--- rcc/web/src/main.ts
+++ rcc/web/src/main.ts
@@ -1,24 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import OpenAPIClientAxios from 'openapi-client-axios'
-import type { Client as ApiClient } from '@/types/openapi'
+import { useApiClient, UseApiClientKey } from './core/api-client'
 
 const test = async () => {
-  const apiPrefix = import.meta.env.VITE_API_PREFIX
-  const api = new OpenAPIClientAxios({
-    definition: `/${apiPrefix}/public/openapi.yml`
-  })
-  // NOTE: initを実行するとなぜかopenapi.ymlを2回fetchしてしまう
-  // api.init()
-  const client = await api.getClient<ApiClient>()
-  client.defaults.baseURL = apiPrefix
-  await client.LocationsController_findAll()
+  const app = createApp(App)
+
+  const apiClient = useApiClient()
+  await apiClient.setup()
+  app.provide(UseApiClientKey, apiClient)
+  app.use(router)
+
+  app.mount('#app')
 }
 test()
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
