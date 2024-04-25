82f617a5c13811e159bf9cc00d0f81c638a56c04
Your Name <you@example.com>
2024-04-25 21:46:53 +0900

    座標系を変換するともとに戻し時にずれるのでへんかんしない

--- src/analysis/column_generater_module/street_view_url_list.py
+++ src/analysis/column_generater_module/street_view_url_list.py
@@ -1,39 +1,30 @@
 from geopandas import GeoDataFrame
 from pandas import Series
 from shapely.geometry import LineString, Point
-from pyproj import Transformer
+from geopy.distance import geodesic
 
 
 def generate(gdf: GeoDataFrame) -> Series:
     def func(row):
-        segments = interpolate_points(row.geometry, 250)
-        # return "\n".join(
-        #     [
-        #         f"https://www.google.com/maps/@{segment.y},{segment.x},20?layer=c&cbll={segment.y},{segment.x}&cbp=12,0,0,0,0"
-        #         for segment in segments
-        #     ],
-        # )
-        return [[segment.y, segment.x] for segment in segments]   
+        segments = interpolate_points(row.geometry, 250, row.length)
+        return [[segment[1], segment[0]] for segment in segments]   
 
     series = gdf.apply(func, axis=1)
     return series
 
 
-def interpolate_points(line: LineString, interval: int) -> list[list[Point, Point]]:
-    # lineStringを平面直角座標系に変換
-    transformer = Transformer.from_crs(4326, 6677)
-    # LineString内のpyprojが扱える形式に変換(y(経度), x(緯度))する
-    trans_coords = transformer.itransform(line.coords, switch=True)
-    line = LineString(trans_coords)
-    length = line.length
-
-    points = [line.interpolate(0)]
-    for dist in range(0, int(length), interval):
-        dist = (dist + interval) if (dist + interval) < length else length
-        point = line.interpolate(dist)
-        points.append(point)
-    # print(points)
-    # 平面直角座標系から緯度経度系に変換
-    transformer = Transformer.from_crs(6677, 4326, always_xy=True)
-    points = [Point(transformer.transform(p.x, p.y)) for p in points]
+def interpolate_points(line: LineString, interval: int, length:int) -> list[list[Point, Point]]:
+    points = [line.coords[0]]
+    distance = 0
+    for index, point in enumerate(line.coords):
+        # x, y = point
+        if index + 1 >= len(line.coords):
+            if distance != 0:
+                points.append(line.coords[-1])
+            continue
+        next_point = line.coords[index + 1]
+        distance += geodesic((point[1], point[0]), (next_point[1], next_point[0])).meters
+        if distance > interval:
+            points.append(next_point)
+            distance = 0
     return points
