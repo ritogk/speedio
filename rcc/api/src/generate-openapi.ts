7d31db74a30d2de0aa574e450702e9114598e7f4
Your Name <you@example.com>
2024-04-28 11:13:45 +0900

    Revert "openapiの自動更新"
    This reverts commit 6c41c6d5dd9ca3605688195442447d6970864e8f.

--- rcc/api/src/generate-openapi.ts
+++ rcc/api/src/generate-openapi.ts
@@ -1,13 +1,16 @@
-import { INestApplication } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as yaml from 'js-yaml';
 
-export const setup = async (app: INestApplication) => {
+export const generate = async () => {
+  const app = await NestFactory.create(AppModule);
   const documentBuilder = new DocumentBuilder()
     .setDescription('The cats API description')
     .setVersion('1.0')
+    // .addServer('https://localhost:5173/api', 'Development server')
     .build();
   const document = SwaggerModule.createDocument(app, documentBuilder);
   // swagger-viewerの起動
@@ -26,3 +29,5 @@ export const setup = async (app: INestApplication) => {
   );
   await fs.writeFile(yamlPath, yamlDocument);
 };
+
+(async () => await generate())();
