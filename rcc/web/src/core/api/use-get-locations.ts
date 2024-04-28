61af78fd13a7901833970d7f2fc816c25481d05b
Your Name <you@example.com>
2024-04-28 17:51:08 +0900

    geomeryからbouingbox生成

--- rcc/web/src/core/api/use-get-locations.ts
+++ rcc/web/src/core/api/use-get-locations.ts
@@ -1,15 +1,25 @@
 import { UseApiClientKey, type UseApiClientType } from '@/core/api/api-client'
 import { useQuery } from '@tanstack/vue-query'
 import { GET_LOCATIONS } from './query-key'
-import { inject } from 'vue'
+import { inject, ref } from 'vue'
+
+import type { Paths } from '@/types/openapi'
 
 export const useGetLocations = () => {
   const useApiClient = inject(UseApiClientKey) as UseApiClientType
   const client = useApiClient.getClient()
-  return useQuery({
+  const queryParams = ref<Paths.LocationsControllerFindAll.QueryParameters>({}) // リアクティブなクエリパラメーター
+  const { data, isError, isLoading, refetch } = useQuery({
     queryKey: [GET_LOCATIONS],
-    queryFn: () => client.LocationsController_findAll().then((res) => res.data),
-    staleTime: Infinity,
+    queryFn: () => client.LocationsController_findAll(queryParams.value).then((res) => res.data),
+    staleTime: 0, // キャッシュを無効化
     retry: false
   })
+
+  const setQueryParams = (params: Paths.LocationsControllerFindAll.QueryParameters) => {
+    queryParams.value = params
+    refetch()
+  }
+
+  return { data, isError, isLoading, setQueryParams }
 }
