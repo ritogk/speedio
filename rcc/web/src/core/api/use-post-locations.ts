de87675de33596c05689ea288bcfc096e5cd301e
Your Name <you@example.com>
2024-04-28 12:07:13 +0900

    不要コード削除

--- rcc/web/src/core/api/use-post-locations.ts
+++ rcc/web/src/core/api/use-post-locations.ts
@@ -21,38 +21,3 @@ export const usePostLocations = () => {
   })
   return mutation
 }
-
-// import { AuthenticationApi } from "@/core/openapiClient"
-// import { apiConfig } from "@/core/openapi"
-// import { useQueryClient, useMutation } from "@tanstack/vue-query"
-// import { GET_STATUS, GET_YOUTUBE_VIDEO, GET_COMPARISONS } from "./query-key"
-
-// export const usePostAuthenticationLogout = () => {
-//   const queryClient = useQueryClient()
-//   const authenticationApi = new AuthenticationApi(apiConfig)
-
-//   const mutation = useMutation({
-//     mutationFn: () => authenticationApi.authenticationLogoutPost(),
-//     onSuccess: async () => {
-//       await queryClient.invalidateQueries({ queryKey: [GET_STATUS], exact: true })
-//       await queryClient.resetQueries({
-//         queryKey: [GET_YOUTUBE_VIDEO],
-//         exact: true
-//       })
-//       await queryClient.resetQueries({
-//         queryKey: [GET_COMPARISONS],
-//         exact: true
-//       })
-//     }
-//   })
-//   return mutation
-// }
-
-// 呼び出し側
-// const postUsers = usePostUsers()
-// await postUsers.mutateAsync({
-//   handleName: form.hanndleName.value.value,
-//   carType: form.carType.value.value,
-//   email: form.email.value.value,
-//   password: form.password.value.value
-// })
