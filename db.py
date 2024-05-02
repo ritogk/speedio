d07ee1bca19f2f9c691ea73cf135180f3434a740
Your Name <you@example.com>
2024-05-02 09:47:07 +0900

    refactor

--- db.py
+++ db.py
@@ -11,10 +11,15 @@ engine = create_engine(f"postgresql://{usename}:{password}@{host}:{port}/{dbname
 # Sessionクラスを作成
 Session = sessionmaker(bind=engine)
 
-# Sessionインスタンスを作成し、データベース操作を行う
-with Session() as session:
-    # SQLクエリを実行
-    result = session.execute(text("SELECT * FROM locations;"))
-    # 結果を表示
-    for row in result:
-        print(row)
+print('start')
+def get_location():
+    # Sessionインスタンスを作成し、データベース操作を行う
+    with Session() as session:
+        # SQLクエリを実行
+        result = session.execute(text("SELECT * FROM locations;"))
+        return result
+locations = get_location()
+# 結果を表示
+for row in locations:
+    print(row)
+print('end')
