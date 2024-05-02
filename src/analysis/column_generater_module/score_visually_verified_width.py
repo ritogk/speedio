e0175cb33884a56939939e57c8f14856cf0190d5
Your Name <you@example.com>
2024-05-02 09:48:06 +0900

    dbの道幅から評価する関数を作成

---
+++ src/analysis/column_generater_module/score_visually_verified_width.py
@@ -0,0 +1,64 @@
+from geopandas import GeoDataFrame
+from pandas import Series
+from .eye_measured_width import RoadCondition
+
+from sqlalchemy import create_engine, text, func
+from sqlalchemy.orm import sessionmaker
+
+# 目視検証データから道幅を評価する
+def generate(gdf: GeoDataFrame) -> Series:
+    usename = "postgres"
+    password = "postgres"
+    dbname = "speedia"
+    host="localhost"
+    port="5432"
+    engine = create_engine(f"postgresql://{usename}:{password}@{host}:{port}/{dbname}")
+    Session = sessionmaker(bind=engine)
+    def func(row):
+        coords = list(row.geometry.coords)
+        min_longitude = coords[0][0]
+        max_longitude = coords[0][0]
+        min_latitude = coords[0][1]
+        max_latitude = coords[0][1]
+
+        for point in coords:
+            if point[0] < min_longitude:
+                min_longitude = point[0]
+            if point[0] > max_longitude:
+                max_longitude = point[0]
+            if point[1] < min_latitude:
+                min_latitude = point[1]
+            if point[1] > max_latitude:
+                max_latitude = point[1]
+        srid = 4326
+        locations = []
+        with Session() as session:
+            # SQLクエリを実行
+            result = session.execute(text(f"SELECT ST_X(point) AS longitude, ST_Y(point) AS latitude, \"roadCondition\"  FROM locations WHERE ST_Intersects(ST_MakeEnvelope({min_longitude}, {min_latitude}, {max_longitude}, {max_latitude}, {srid}), locations.point)"))
+            result = result.fetchall()
+            # print(f"coords: {len(coords)}")
+            # print(f"db_result: {len(rows)}")
+            # geometryの座標に一致するデータのみ取り出す
+            for coord in coords:
+                for data in result:
+                    if data.longitude == coord[0] and data.latitude == coord[1]:
+                        locations.append(data)
+        # 必要最低限の座標データがある場合のみに評価する
+        # 上記の判断式: points / (length/500) ≥ 0.5の場合
+        if (len(locations) / (row.length / 500)) >= 0.5:
+            # ここに入ってこれるって事は十分なデータがあるって事
+            score = 0
+            for location in locations:
+                if location.roadCondition == "TWO_LANE" or location.roadCondition == "TWO_LANE_SHOULDER":
+                    score += 1
+                elif location.roadCondition == "ONE_LANE_SPACIOUS":
+                    score += 0.5
+                elif location.roadCondition == "ONE_LANE":
+                    score += 0.1
+            # print(f"count: {len(locations)} score: {score/len(locations)}")
+            return score/len(locations)
+        else:
+            return 0
+
+    series = gdf.apply(func, axis=1)
+    return series
