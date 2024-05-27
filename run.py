762eea0d87c50e30905fd06ced9a441de288e824
Your Name <you@example.com>
2024-05-27 09:50:55 +0900

    角度が求められるところまで。

--- run.py
+++ run.py
@@ -26,6 +26,7 @@ def run():
             angle, direction = calculate_angle_between_vectors(
                 coords[i - 1], coords[i], coords[i + 1]
             )
+            ## ここstartとかendとかなくてもよいか・・・？
             result = {
                 'start': {'lat': coords[i - 1][1], 'lng': coords[i - 1][0]},
                 'center': {'lat': coords[i][1], 'lng': coords[i][0]},
@@ -34,11 +35,51 @@ def run():
                 'horizontalVector': direction
             }
             results.append(result)
-        return results
+
+        # 同angleのデータが3つ以上連続していないデータを削除する
+        filtered_results = []
+        count = 1
+        for i in range(1, len(results)):
+            if results[i]['horizontalVector'] == results[i - 1]['horizontalVector']:
+                count += 1
+            else:
+                if count >= 3:
+                    filtered_results.extend(results[i - count:i])
+                count = 1
+        # 最後のグループをチェック
+        if count >= 3:
+            filtered_results.extend(results[-count:])
+        return filtered_results
 
     series = gdf.apply(func, axis=1)
 
-    print(series.to_list()[0])
+    target = series.to_list()[0]
+    # 曲がり角毎にまとめる。
+    turns = []
+    old_horizontal_vector = target[0]['horizontalVector']
+    turn = [target[0]]
+    for i in range(1, len(target)):
+        if target[i]['horizontalVector'] == old_horizontal_vector:
+            turn.append(target[i])
+        else:
+            turns.append(turn)
+            turn = [target[i]]
+            old_horizontal_vector = target[i]['horizontalVector']
+    turns.append(turn)
+    
+    # turnの始点と最大角度の座標と終点の間の角度を求める。
+    for turn in turns:
+        max_angle = 0
+        max_angle_point = max(turn, key=lambda x: x['angle'])
+        # ここでmに変換する必要はなにのか・・？
+        angle, direction = calculate_angle_between_vectors(
+                (turn[0]['start']['lng'], turn[0]['start']['lat']),
+                (max_angle_point['center']['lng'], max_angle_point['center']['lat']),
+                (turn[-1]['end']['lng'], turn[-1]['end']['lat'])
+            )
+        print(angle, direction, (max_angle_point['center']['lng'], max_angle_point['center']['lat']))
+
+    # print(turns)
     # matplotlibを使用して描画
     fig, ax = plt.subplots()
     gpd.GeoSeries(first_geometry).plot(ax=ax)
