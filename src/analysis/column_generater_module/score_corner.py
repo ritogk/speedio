8484e8e2077acd7f834d0a115386baab8c78c8db
Your Name <you@example.com>
2024-06-02 11:23:39 +0900

    medium_speed_cornerの範囲を広げた

--- src/analysis/column_generater_module/score_corner.py
+++ src/analysis/column_generater_module/score_corner.py
@@ -2,8 +2,8 @@ from geopandas import GeoDataFrame
 from pandas import Series
 
 HIGHT_SPEED_CORNER_ANGLE_MIN = 25
-HIGHT_SPEED_CORNER_ANGLE_MAX = 45
-MEDIUM_SPEED_CORNER_ANGLE_MIN = 46
+HIGHT_SPEED_CORNER_ANGLE_MAX = 41
+MEDIUM_SPEED_CORNER_ANGLE_MIN = 42
 MEDIUM_SPEED_CORNER_ANGLE_MAX = 60
 LOW_SPEED_CORNER_ANGLE_MIN = 61
 
@@ -11,15 +11,15 @@ def generate(gdf: GeoDataFrame) -> tuple[Series, Series, Series]:
     def func(x):
         # 高速コーナーのスコア計算
         score_high_speed_corner = sum(
-            item['distance'] for item in x.corners if 25 <= item['angle'] <= 45
+            item['distance'] for item in x.corners if HIGHT_SPEED_CORNER_ANGLE_MIN <= item['angle'] <= HIGHT_SPEED_CORNER_ANGLE_MAX
         ) / x.length
         # 中速コーナーのスコア計算
         score_medium_speed_corner = sum(
-            item['distance'] for item in x.corners if 46 <= item['angle'] <= 60
+            item['distance'] for item in x.corners if MEDIUM_SPEED_CORNER_ANGLE_MIN <= item['angle'] <= MEDIUM_SPEED_CORNER_ANGLE_MAX
         ) / x.length
         # 低速コーナーのスコア計算
         score_low_speed_corner = sum(
-            item['distance'] for item in x.corners if item['angle'] >= 61
+            item['distance'] for item in x.corners if item['angle'] >= LOW_SPEED_CORNER_ANGLE_MIN
         ) / x.length
 
         return score_high_speed_corner, score_medium_speed_corner, score_low_speed_corner
