cb60e295d52840f961ac0e443b405797dc6bd97f
ritogk <your.email@example.com>
2024-07-02 08:55:06 +0900

    googleマップのルートを精度をあげた

--- src/analysis/column_generater_module/google_map_url.py
+++ src/analysis/column_generater_module/google_map_url.py
@@ -1,10 +1,13 @@
-from geopandas import GeoDataFrame
+from geopandas import GeoDataFrame, GeoSeries
 from pandas import Series
 
 
+# 開始、中央、終了地点からgoogleMapのルートURLを作成する
 def generate(gdf: GeoDataFrame) -> Series:
-    def func(row):
-        return f"https://www.google.co.jp/maps/dir/{row.start_point[1]},{row.start_point[0]}/'{row.end_point[1]},{row.end_point[0]}'"
+    def func(row: GeoSeries):
+        coords = row.geometry.coords
+        center_point = coords[len(coords) // 2]
+        return f"https://www.google.co.jp/maps/dir/{row.start_point[1]},{row.start_point[0]}/{center_point[1]},{center_point[0]}/{row.end_point[1]},{row.end_point[0]}"
 
     series = gdf.apply(func, axis=1)
     return series
