51471d8449797fa01d008c691e5b4803755274a7
ritogk <your.email@example.com>
2024-07-02 08:44:50 +0900

    コーナーの評価の距離の扱いを変更

--- src/analysis/column_generater_module/score_corner.py
+++ src/analysis/column_generater_module/score_corner.py
@@ -6,20 +6,26 @@ HIGHT_SPEED_CORNER_ANGLE_MAX = 41
 MEDIUM_SPEED_CORNER_ANGLE_MIN = 42
 MEDIUM_SPEED_CORNER_ANGLE_MAX = 60
 LOW_SPEED_CORNER_ANGLE_MIN = 61
+# 現状のアルゴリズムの都合上、1000mのコーナーが存在してしまい、直線区間がなくコーナーの性質が薄いため450mとして帳尻を合わせる。
+# NOTE: https://www.notion.so/d2fe2f7ad1be47a9831863f20a83c0ac?pvs=4
+MAX_DISTANCE = 450  # 最大距離
 
 def generate(gdf: GeoDataFrame) -> tuple[Series, Series, Series]:
     def func(x):
         # 高速コーナーのスコア計算
         score_high_speed_corner = sum(
-            item['distance'] for item in x.corners if HIGHT_SPEED_CORNER_ANGLE_MIN <= item['angle'] <= HIGHT_SPEED_CORNER_ANGLE_MAX
+            min(item['distance'], MAX_DISTANCE) for item in x.corners
+            if (HIGHT_SPEED_CORNER_ANGLE_MIN <= item['angle'] <= HIGHT_SPEED_CORNER_ANGLE_MAX)
         ) / x.length
         # 中速コーナーのスコア計算
         score_medium_speed_corner = sum(
-            item['distance'] for item in x.corners if MEDIUM_SPEED_CORNER_ANGLE_MIN <= item['angle'] <= MEDIUM_SPEED_CORNER_ANGLE_MAX
+            min(item['distance'], MAX_DISTANCE) for item in x.corners
+            if (MEDIUM_SPEED_CORNER_ANGLE_MIN <= item['angle'] <= MEDIUM_SPEED_CORNER_ANGLE_MAX)
         ) / x.length
         # 低速コーナーのスコア計算
         score_low_speed_corner = sum(
-            item['distance'] for item in x.corners if item['angle'] >= LOW_SPEED_CORNER_ANGLE_MIN
+            min(item['distance'], MAX_DISTANCE) for item in x.corners
+            if item['angle'] >= LOW_SPEED_CORNER_ANGLE_MIN
         ) / x.length
 
         return score_high_speed_corner, score_medium_speed_corner, score_low_speed_corner
