7d61784d19c71834ceb8ed2502bc703d6931440c
ritogk <your.email@example.com>
2024-07-15 12:17:58 +0900

    エッジの表示切り替えロジックを作成

--- run.py
+++ run.py
@@ -5,42 +5,43 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 from src.analysis.column_generater_module.core.calculate_angle_between_vectors import calculate_angle_between_vectors
 from geopy.distance import geodesic
+from src.core.env import getEnv
 
 def run():
     gdf = main()
-    # 1行目のgeometryをグラフに表示
-    # 'geometry'列の1行目を選択
 
-    # 先頭のdataframeをセットする
-    first_geometry = gdf.iloc[0].geometry
-    datas = gdf.iloc[0].corners
-    
-    # matplotlibを使用して描画
-    fig, ax = plt.subplots()
-    
-    # first_geometryを背面に表示
-    gpd.GeoSeries(first_geometry).plot(ax=ax, color='black', alpha=0.01)
-    
-     # カラーマップと正規化
-    cmap_right = plt.get_cmap('Reds')
-    cmap_left = plt.get_cmap('Blues')
-    norm = plt.Normalize(vmin=min(data['angle'] for data in datas),
-                         vmax=max(data['angle'] for data in datas))
+    env = getEnv()
+    if env["SHOW_HIGH_EVALUATION"]:
+        # 先頭のdataframeをセットする
+        first_geometry = gdf.iloc[0].geometry
+        datas = gdf.iloc[0].corners
+        
+        # matplotlibを使用して描画
+        fig, ax = plt.subplots()
+        
+        # first_geometryを背面に表示
+        gpd.GeoSeries(first_geometry).plot(ax=ax, color='black', alpha=0.01)
+        
+        # カラーマップと正規化
+        cmap_right = plt.get_cmap('Reds')
+        cmap_left = plt.get_cmap('Blues')
+        norm = plt.Normalize(vmin=min(data['angle'] for data in datas),
+                            vmax=max(data['angle'] for data in datas))
 
-    # turnsのpointsを表示する
-    for data in datas:
-        points = data['points']
-        if data['direction'] == 'right':
-            color = cmap_right(norm(data['angle']))
-        elif data['direction'] == 'left':
-            color = cmap_left(norm(data['angle']))
-        else:
-            color = 'grey'  # その他の方向の場合はグレー
-        lat, lng = zip(*[(point['lat'], point['lng']) for point in points])
-        ax.plot(lng, lat, color=color, linewidth=2)  # sはマーカーのサイズ
+        # turnsのpointsを表示する
+        for data in datas:
+            points = data['points']
+            if data['direction'] == 'right':
+                color = cmap_right(norm(data['angle']))
+            elif data['direction'] == 'left':
+                color = cmap_left(norm(data['angle']))
+            else:
+                color = 'grey'  # その他の方向の場合はグレー
+            lat, lng = zip(*[(point['lat'], point['lng']) for point in points])
+            ax.plot(lng, lat, color=color, linewidth=2)  # sはマーカーのサイズ
 
-    plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap_right), ax=ax, label='Angle (degrees)')
-    plt.show()
+        plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap_right), ax=ax, label='Angle (degrees)')
+        plt.show()
 
     return gdf
 
--- src/core/env.py
+++ src/core/env.py
@@ -9,8 +9,10 @@ def getEnv():
     POINT_ST = os.getenv("POINT_ST")
     POINT_ED = os.getenv("POINT_ED")
     CONSIDER_GSI_WIDTH = os.getenv("CONSIDER_GSI_WIDTH")
+    SHOW_HIGH_EVALUATION = os.getenv("SHOW_HIGH_EVALUATION")
     return {
         "POINT_ST": POINT_ST.replace(" ", "").split(","),
         "POINT_ED": POINT_ED.replace(" ", "").split(","),
         "CONSIDER_GSI_WIDTH": True if CONSIDER_GSI_WIDTH == "1" else False,
+        "SHOW_HIGH_EVALUATION": True if SHOW_HIGH_EVALUATION == "1" else False,
     }
