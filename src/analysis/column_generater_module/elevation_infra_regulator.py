e4a857300fc41b793082adc47412ac62070a9f2f
ritogk <your.email@example.com>
2024-07-15 11:57:23 +0900

    橋とトンネルの調整ロジックを統一

--- src/analysis/column_generater_module/elevation_infra_regulator.py
+++ src/analysis/column_generater_module/elevation_infra_regulator.py
@@ -3,46 +3,50 @@ from pandas import Series
 import numpy as np
 from shapely.geometry import Point
 from typing import List, Tuple
+from enum import Enum
 
-# トンネル内の標高を調整する
+class InfraType(Enum):
+    TUNNEL = 1
+    BRIDGE = 2
+
+# トンネルや橋の標高を調整する
 # 問題: 現状標高は地球表面の形状の値なためトンネル区間の標高値が道の値ではなく山の値になってしまっている。
 # また上記の区間はu_shape_elevationも高くなってしまう。
-def generate(gdf: GeoDataFrame, tunnel_edges: GeoDataFrame) -> Series:
+def generate(gdf: GeoDataFrame, infra_edges: GeoDataFrame, infraType: InfraType) -> Series:
     # トンネルの空間インデックスを作成
-    tunnel_edges_sindex = tunnel_edges["geometry"].sindex
+    infra_edges_sindex = infra_edges["geometry"].sindex
     def func(row: GeoSeries):
-        # tunnelのパターンは大きく分けて2つで'yes'とNan
-        if row['bridge'] != 'yes':
+        if (infraType == InfraType.BRIDGE and row['bridge'] != 'yes') or (infraType == InfraType.TUNNEL and row['tunnel'] != 'yes'):
             return row['elevation']
         base_edge_coords = list(row.geometry.coords) 
         # 1. 対象のエッジのバウンディングボックス内のトンネルのインデックスを取得(バウンディングボックスでの抽出なのでエッジ外のトンネルも含まれる可能性がある)
-        tunnels_edge_in_bbox_index_list = list(tunnel_edges_sindex.intersection(row.geometry.bounds))
+        infra_edge_in_bbox_index_list = list(infra_edges_sindex.intersection(row.geometry.bounds))
         # 2. インデックスからトンネルのエッジを取得
-        tunnels_edges_in_bbox = tunnel_edges.iloc[tunnels_edge_in_bbox_index_list]
+        infra_edges_in_bbox = infra_edges.iloc[infra_edge_in_bbox_index_list]
         # 3. 対象のエッジと重なるトンネルのみを抽出(一旦座標が2以上重なっているかで判断)
-        target_tunnels_index_list = []
-        for idx, tunnel_edge in tunnels_edges_in_bbox.iterrows():
+        target_infra_index_list = []
+        for idx, infra_edge in infra_edges_in_bbox.iterrows():
             # base_edge_coordsと一致する座標数を取得
-            num_match_coords = sum([1 for coord in tunnel_edge.geometry.coords if coord in base_edge_coords])
+            num_match_coords = sum([1 for coord in infra_edge.geometry.coords if coord in base_edge_coords])
             if num_match_coords >= 2:
-                target_tunnels_index_list.append(idx)
-        if len(target_tunnels_index_list) == 0:
+                target_infra_index_list.append(idx)
+        if len(target_infra_index_list) == 0:
             # print("★ 対象のトンネルなし。多分対象のエッジ外のトンネルしかない状態だと思う。")
             return row['elevation']
-        target_tunnels_edges = tunnels_edges_in_bbox.loc[target_tunnels_index_list]
+        target_infra_edges = infra_edges_in_bbox.loc[target_infra_index_list]
 
-        for i, tunnel_edge in target_tunnels_edges.iterrows():
-            coords = list(tunnel_edge.geometry.coords)
+        for i, infra_edge in target_infra_edges.iterrows():
+            coords = list(infra_edge.geometry.coords)
             # 先頭と末尾の座標を表示
             print(f'st: {coords[0]} ed: {coords[-1]}')
         
         # トンネルの始点と終点が線形になるように標高を調整
         elevation_adjusted = row.elevation.copy()
-        for i, tunnel_edge in target_tunnels_edges.iterrows():
-            tunnel_coords = list(tunnel_edge.geometry.coords)       
+        for i, infra_edge in target_infra_edges.iterrows():
+            infra_coords = list(infra_edge.geometry.coords)       
             # トンネルの始点と終点に最も近い道のトンネル外の座標を取得
-            nearest_outside_start = get_nearest_outside_point(row, tunnel_coords[0], tunnel_coords)
-            nearest_outside_end = get_nearest_outside_point(row, tunnel_coords[-1], tunnel_coords)
+            nearest_outside_start = get_nearest_outside_point(row, infra_coords[0], infra_coords)
+            nearest_outside_end = get_nearest_outside_point(row, infra_coords[-1], infra_coords)
             a_idx = base_edge_coords.index(nearest_outside_start)
             b_idx = base_edge_coords.index(nearest_outside_end)
             start_idx = min(a_idx, b_idx)
@@ -68,12 +72,12 @@ def generate(gdf: GeoDataFrame, tunnel_edges: GeoDataFrame) -> Series:
     return results
 
 # トンネル外で最も近い座標を取得
-def get_nearest_outside_point(road_edge: GeoSeries, point: Tuple[float, float] , tunnel_coords: list):
+def get_nearest_outside_point(road_edge: GeoSeries, point: Tuple[float, float] , infra_coords: list):
     road_coords = list(road_edge.geometry.coords)
-    # 単純にベースのエッジ(roadからtunnelの座標をすべて消して、指定のトンネルの座標に最も近い座標を取得すればいいだけ)
-    # 1. road_edgeからtunnel_coordsの座標をすべて消す ※1
-    road_coords_without_tunnels = [coord for coord in road_coords if not coord in tunnel_coords]
+    # 単純にベースのエッジ(roadからinfraの座標をすべて消して、指定のトンネルの座標に最も近い座標を取得すればいいだけ)
+    # 1. road_edgeからinfra_coordsの座標をすべて消す ※1
+    road_coords_without_infras = [coord for coord in road_coords if not coord in infra_coords]
     # 2. ※1から指定座標(point)に最も近い座標を取得 ※3
-    nearest_point = min(road_coords_without_tunnels, key=lambda x: Point(x).distance(Point(point)))
+    nearest_point = min(road_coords_without_infras, key=lambda x: Point(x).distance(Point(point)))
     # 3. ※3を返す
     return nearest_point
