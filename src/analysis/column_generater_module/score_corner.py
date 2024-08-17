6ed780290b7a25d301d175ddb71e7ae776e6e8d3
ritogk <your.email@example.com>
2024-08-17 14:31:36 +0900

    fix: コーナー評価の距離の扱いを修正

--- src/analysis/column_generater_module/score_corner.py
+++ src/analysis/column_generater_module/score_corner.py
@@ -1,5 +1,6 @@
 from geopandas import GeoDataFrame
 from pandas import Series
+from geopy.distance import geodesic
 
 WEEK_CORNER_ANGLE_MIN = 22
 WEEK_CORNER_ANGLE_MAX = 45
@@ -12,21 +13,25 @@ MAX_DISTANCE = 450  # 最大距離
 
 def generate(gdf: GeoDataFrame) -> tuple[Series, Series, Series]:
     def func(x):
+        # コーナーと距離の割合にx.lengthを使ってしまうとコーナーの合計距離よりも小さい値になってしまう。
+        # これは3点の座標を1つのコーナーとして扱っているため、範囲が重なる部分があるためである。
+        all_corner_distance = sum(item['distance'] for item in x.corners) 
+
         # 弱コーナーのスコア計算
         score_week_corner = sum(
             min(item['distance'], MAX_DISTANCE) for item in x.corners
             if (WEEK_CORNER_ANGLE_MIN <= item['max_steering_angle'] < WEEK_CORNER_ANGLE_MAX)
-        ) / x.length
+        ) / all_corner_distance
         # 中コーナーのスコア計算
         score_medium_corner = sum(
             min(item['distance'], MAX_DISTANCE) for item in x.corners
             if (MEDIUM_CORNER_ANGLE_MIN <= item['max_steering_angle'] < MEDIUM_CORNER_ANGLE_MAX)
-        ) / x.length
+        ) / all_corner_distance
         # 強コーナーのスコア計算
         score_strong_corner = sum(
             min(item['distance'], MAX_DISTANCE) for item in x.corners
             if STRONG_CORNER_ANGLE_MIN <= item['max_steering_angle']
-        ) / x.length
+        ) / all_corner_distance
 
         return score_week_corner, score_medium_corner, score_strong_corner
 
