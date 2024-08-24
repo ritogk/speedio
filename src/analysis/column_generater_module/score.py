fd9035145096a2babf79559bcfa3b5969d7790b7
ritogk <your.email@example.com>
2024-08-24 20:34:21 +0900

    feat: スコア毎にソート

--- src/analysis/column_generater_module/score.py
+++ src/analysis/column_generater_module/score.py
@@ -7,10 +7,10 @@ WEIGHTS = {
     "elvation_over_height": 1.0,  
     "elevation_u_shape": 1.0,  
     "angle": 0,  
-    "width": 0,
+    "width": 1,
     "length": 0.6,
-    "week_corner": 0,
-    "medium_corner": 0,
+    "week_corner": 1,
+    "medium_corner": 1,
     "strong_corner": 1,
 }
 
@@ -20,17 +20,31 @@ WEIGHTS = {
 def generate(gdf: GeoDataFrame, corner_type: str) -> Series:
     def func(x):
         # スコア計算
-        return (
-            x["score_elevation"] * WEIGHTS["elevation"]
-            + (1 - x["score_elevation_over_heiht"] * WEIGHTS["elvation_over_height"])
-            + x["score_elevation_u_shape"] * WEIGHTS["elevation_u_shape"]
-            + x["score_angle"] * WEIGHTS["angle"]
-            + x["score_width"] * WEIGHTS["width"]
-            + x["score_length"] * WEIGHTS["length"]
-            + x["score_week_corner"] * (2 if corner_type == 'week_corner' else 1)
-            + x["score_medium_corner"] * (2 if corner_type == 'medium_corner' else 1)
-            + x["score_strong_corner"] * (2 if corner_type == 'strong_corner' else 1)
-        ) / len(WEIGHTS)
+        if(corner_type == 'elevation_fluctuation'):
+            return (
+                x["score_elevation"] * WEIGHTS["elevation"]
+                + (1 - x["score_elevation_over_heiht"] * WEIGHTS["elvation_over_height"])
+                + x["score_elevation_u_shape"] * WEIGHTS["elevation_u_shape"]
+                + x["score_angle"] * WEIGHTS["angle"]
+                + x["score_width"] * WEIGHTS["width"]
+                + x["score_length"] * WEIGHTS["length"]
+                + x["score_week_corner"] * WEIGHTS["week_corner"]
+                + x["score_medium_corner"] * WEIGHTS["medium_corner"]
+                + x["score_strong_corner"] * WEIGHTS["strong_corner"]
+            ) / len(WEIGHTS)
+        else:
+            # csv出力用
+            return (
+                x["score_elevation"] * WEIGHTS["elevation"]
+                + (1 - x["score_elevation_over_heiht"] * WEIGHTS["elvation_over_height"])
+                + x["score_elevation_u_shape"] * WEIGHTS["elevation_u_shape"]
+                + x["score_angle"] * WEIGHTS["angle"]
+                + x["score_width"] * WEIGHTS["width"]
+                + x["score_length"] * WEIGHTS["length"]
+                + x["score_week_corner"] * (2 if corner_type == 'week_corner' else 1)
+                + x["score_medium_corner"] * (2 if corner_type == 'medium_corner' else 1)
+                + x["score_strong_corner"] * (2 if corner_type == 'strong_corner' else 1)
+            ) / len(WEIGHTS)
 
     series = gdf.apply(func, axis=1)
     return series
