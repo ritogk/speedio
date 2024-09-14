29805238cdeef86778e963ecef8fa4ea97753532
ritogk <your.email@example.com>
2024-09-14 09:45:49 +0900

    fix: 0件の処理を追加

--- src/analysis/gdf_building_feather.py
+++ src/analysis/gdf_building_feather.py
@@ -4,17 +4,20 @@ from geopandas import GeoDataFrame
 # 建物のジオメトリーを取得する
 def fetch_gdf(
     latitude_start, longitude_start, latitude_end, longitude_end
-) -> GeoDataFrame:
+) -> GeoDataFrame | None:
     # キャッシュを使う
     ox.settings.use_cache = True
     ox.settings.log_console = False
-
-    gdf_buildings = ox.features_from_bbox(
-        north=max(latitude_start, latitude_end),
-        south=min(latitude_start, latitude_end),
-        east=max(longitude_start, longitude_end),
-        west=min(longitude_start, longitude_end),
-        tags={'building': True}
-    )
+    try:
+        gdf_buildings = ox.features_from_bbox(
+            north=max(latitude_start, latitude_end),
+            south=min(latitude_start, latitude_end),
+            east=max(longitude_start, longitude_end),
+            west=min(longitude_start, longitude_end),
+            tags={'building': True}
+        )
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+        return None
 
     return gdf_buildings
