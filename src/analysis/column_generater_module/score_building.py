31ea02045cdc27165bec25acd92abfcef78f8365
ritogk <your.email@example.com>
2024-09-24 10:49:03 +0900

    feat: 建物の重みを調整

--- src/analysis/column_generater_module/score_building.py
+++ src/analysis/column_generater_module/score_building.py
@@ -1,8 +1,8 @@
 from geopandas import GeoDataFrame
 from pandas import Series
 
-# 一旦1建物の横幅の長さを30mとする。
-BUILDING_LENGTH = 30
+# 一旦1建物の横幅の長さを35mとする。
+BUILDING_LENGTH = 35
 
 # 近くに建物が少ないほどスコアが高くなる
 def generate(gdf: GeoDataFrame) -> tuple[Series, Series, Series]:
