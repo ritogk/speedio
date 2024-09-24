cc96da37edeb69743941fa863e19a33065125ac0
ritogk <your.email@example.com>
2024-09-24 10:22:23 +0900

    fix: 不具合修正

--- src/analysis/column_generater_module/score_elevation_deviation.py
+++ src/analysis/column_generater_module/score_elevation_deviation.py
@@ -7,7 +7,10 @@ def generate(gdf: GeoDataFrame) -> Series:
         elevation_fluctuation_up, elevation_fluctuation_down = row["elevation_fluctuation"]
         min_val = min(elevation_fluctuation_up, elevation_fluctuation_down)
         max_val = max(elevation_fluctuation_up, elevation_fluctuation_down)
-
+        
+        # なんかたまに0になるのでその対策
+        if min_val == 0 or max_val == 0:
+            return 0
         return min_val / max_val
 
     series = gdf.apply(func, axis=1)
