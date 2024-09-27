67e8df89ec9b0587333d57d999e08657e4964610
ritogk <your.email@example.com>
2024-09-27 22:17:37 +0900

    fix: 2000m以下のジオメトリーを削除

--- src/analysis/remover_module/filter_edge.py
+++ src/analysis/remover_module/filter_edge.py
@@ -3,12 +3,12 @@ import numpy as np
 
 
 # 以下の条件に該当するエッジを峠の候補にする
-# エッジの長さが1000m以上
+# エッジの長さが2000m以上
 # ❌️エッジの長さが10000m以下
 # ジオメトリーの座標感の角度変化量の合計が120度以上
 # 1500mで10分岐以下
 def remove(gdf: GeoDataFrame) -> GeoDataFrame:
-    lower_bound_meter = 1000
+    lower_bound_meter = 2000
     # max_bound_meter = 10000
     branch_meter_rate = 10 / 1500  # 1500mで10分岐以上ある場合は対象外にする
 
