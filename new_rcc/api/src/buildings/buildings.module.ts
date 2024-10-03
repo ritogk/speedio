b5dcbae37fd39f7f89c43f2e7905a6269e87deaf
ritogk <your.email@example.com>
2024-10-03 20:04:43 +0900

    fix: ディレクトリ整理

--- new_rcc/api/src/buildings/buildings.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Building } from './entities/building.entity';
-@Module({
-  imports: [TypeOrmModule.forFeature([Building])],
-})
-export class BuildingsModule {}
--- rcc/web/src/router/index.ts
+++ rcc/web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../pages/Home.vue'
+import SubView from '../pages/Sub.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/sub',
+      name: 'sub',
+      component: SubView
     }
   ]
 })
