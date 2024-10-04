36cf96cd3a416671d4b3fb2199048c73945a8945
ritogk <your.email@example.com>
2024-10-04 12:04:21 +0900

    feat: 凹凸評価の精度を向上

--- src/analysis/column_generater_module/score_elevation_peak.py
+++ src/analysis/column_generater_module/score_elevation_peak.py
@@ -6,15 +6,15 @@ from ...core.convert_range import convert_range
 def generate(gdf: GeoDataFrame) -> Series:
     def func(x):
         elevation_peak_count = x.elevation_peak_count
-        # 1-6の範囲を(0.5-1)に変換する。
+        # 1-10の範囲を(0.3-1)に変換する。
+        max = 10
         if elevation_peak_count <= 0:
             return 0
-        elif elevation_peak_count >= 6:
+        elif elevation_peak_count >= max:
             return 1
         
-        # 1-6の範囲を(0.5-1)に変換する。
-        old_min, old_max = 1, 6
-        new_min, new_max = 0.5, 1
+        old_min, old_max = 1, max
+        new_min, new_max = 0.3, 1
         converted_value = convert_range(elevation_peak_count, old_min, old_max, new_min, new_max)
         return converted_value
 
