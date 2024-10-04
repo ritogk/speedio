be98e548415c34f367ada7535eedca1feff4ff2a
ritogk <your.email@example.com>
2024-10-04 15:29:06 +0900

    fix: 重み調整

--- src/analysis/column_generater_module/score_corner_level_deviation.py
+++ src/analysis/column_generater_module/score_corner_level_deviation.py
@@ -2,24 +2,30 @@ from geopandas import GeoDataFrame
 from pandas import Series
 import numpy as np
 
+WEEK_WEIGHT = 1
+MEDIUM_WEIGHT = 1
+STRONG_WEIGHT = 1.15 # 強コーナーは印象が強いので重みで上げる
+NONE_WEIGHT = 1
 # 目視検証データから道幅を評価する
 def generate(gdf: GeoDataFrame) -> Series:
     def func(row):
-        score_corner_week = row.score_corner_week
-        score_corner_medium = row.score_corner_medium
-        score_corner_strong = row.score_corner_strong
-        score_corner_none = row.score_corner_none
-        # a, b, c, d の4つの値
+        score_corner_week = row.score_corner_week * WEEK_WEIGHT
+        score_corner_medium = row.score_corner_medium * MEDIUM_WEIGHT
+        score_corner_strong = row.score_corner_strong * STRONG_WEIGHT
+        score_corner_none = row.score_corner_none * NONE_WEIGHT
+        
         values = [score_corner_week, score_corner_medium, score_corner_strong, score_corner_none]
 
-        max_val = max(values)
-        min_val = min(values)
+        # 正規化定数 k を設定（必要に応じて調整）大きくするほど標準偏差の影響が小さくなる。
+        k = 0.8
 
-        # 正規化定数 k を設定（必要に応じて調整）
-        k = 1.0
+        # 標準偏差を計算
+        std_dev = np.std(values)
+        # print(std_dev)
 
         # 評価値の計算
-        score = 1 - (max_val - min_val) / k
+        score = 1 - (std_dev / k)
+        # print(score)
 
         return score
 
