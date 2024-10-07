0dcf6c500d899db66963ecb4448af10379adaf9b
ritogk <your.email@example.com>
2024-10-07 18:10:50 +0900

    fix: バグ修正

--- src/analysis/column_generater_module/angle_deltas.py
+++ src/analysis/column_generater_module/angle_deltas.py
@@ -1,22 +1,24 @@
 from geopandas import GeoDataFrame
 from pandas import Series
-import osmnx as ox
-import numpy as np
 
 from .core.calculate_angle_between_vectors import calculate_angle_between_vectors
 
 
 def generate(gdf: GeoDataFrame) -> Series:
     # 座標間の角度の変化の合計値を求める
-    series = gdf["geometry"].apply(
-        lambda x: sum(
-            [
-                calculate_angle_between_vectors(
-                    x.coords[i - 1], x.coords[i], x.coords[i + 1]
-                )[0]
-                or 0
-                for i in range(1, len(x.coords) - 1)
-            ]
-        )
-    )
+    def func(row):
+        coords = list(row.geometry.coords)
+        angle_total = 0
+        for i in range(1, len(coords) - 1):
+            result = calculate_angle_between_vectors(
+                            coords[i - 1], coords[i], coords[i + 1]
+                        )
+            if result is None:
+                continue
+            else:
+                angle = result[0]
+                angle_total += angle
+        return angle_total
+
+    series = gdf.apply(func, axis=1)
     return series
