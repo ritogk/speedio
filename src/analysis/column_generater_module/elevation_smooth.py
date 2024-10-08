319ee830756987b57aa475e1c1e1dd4d944cade6
ritogk <your.email@example.com>
2024-10-08 20:26:03 +0900

    feat: 共通化

--- src/analysis/column_generater_module/elevation_smooth.py
+++ src/analysis/column_generater_module/elevation_smooth.py
@@ -1,27 +1,15 @@
 from geopandas import GeoDataFrame
 from pandas import Series
 import pandas as pd
+from .core.smoother import generate_moving_average
 
-
+WINDOW_SIZE = 5
 # 標高を平準化する
 def generate(gdf: GeoDataFrame) -> Series:
-    window_size = 5
 
     def func(row):
         series = pd.Series(row.elevation)
-        # 移動平均でサンプリング4で平滑化する。平準化の影響で3つのデータが削除される。
-        smooth = series.rolling(window=window_size).mean().dropna().tolist()
-        # 末尾のデータを追加することで、平準化によって削除されたデータを補完する。
-        # 最後から3番目のデータは、最後から3番目以降のデータの平均とする。
-        # 最後から2番目のデータは、最後から2番目と最後のデータの平均とする。
-        # 最後のデータは平均せずそのまま使用する。
-        if len(series) >= 4:
-            last4_avg = series.iloc[-4:].mean()
-            last3_avg = series.iloc[-3:].mean()
-            last2_avg = series.iloc[-2:].mean()
-            last1 = series.iloc[-1]
-            smooth.extend([last4_avg, last3_avg, last2_avg, last1])
-        return smooth
+        return generate_moving_average(series, WINDOW_SIZE).to_list()
 
     results = gdf.apply(func, axis=1)
     return results
