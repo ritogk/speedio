82b79ac312fe048f78d8fc672b01ff832df9fd70
ritogk <your.email@example.com>
2024-10-08 17:46:51 +0900

    feat: グラフを線に変更

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from src.core.env import getEnv
 from src.analysis.column_generater_module.score_corner_level import WEEK_CORNER_ANGLE_MIN, WEEK_CORNER_ANGLE_MAX, MEDIUM_CORNER_ANGLE_MIN, MEDIUM_CORNER_ANGLE_MAX, STRONG_CORNER_ANGLE_MIN
-import itertools
 
 def run():
     gdf = main()
@@ -14,46 +13,73 @@ def run():
         # 先頭のdataframeをセットする
         gdf_first = gdf.iloc[0]
 
-        corners = gdf_first.road_section
+        road_section = gdf_first.road_section
 
-        week_corner = [item for item in corners if
-                       (WEEK_CORNER_ANGLE_MIN <= item['adjusted_steering_angle'] < WEEK_CORNER_ANGLE_MAX)]
-        medium_corner = [item for item in corners if
-                       (MEDIUM_CORNER_ANGLE_MIN <= item['adjusted_steering_angle'] < MEDIUM_CORNER_ANGLE_MAX)]
-        strong_corner = [item for item in corners if
-                          STRONG_CORNER_ANGLE_MIN <= item['adjusted_steering_angle']]
+        week_corner = [item for item in road_section
+                       if
+                        (WEEK_CORNER_ANGLE_MIN <= item['adjusted_steering_angle'] < WEEK_CORNER_ANGLE_MAX)
+                        and item['section_type'] != 'straight']
+        medium_corner = [item for item in road_section if
+                       (MEDIUM_CORNER_ANGLE_MIN <= item['adjusted_steering_angle'] < MEDIUM_CORNER_ANGLE_MAX)
+                       and item['section_type'] != 'straight']
+        strong_corner = [item for item in road_section if
+                          STRONG_CORNER_ANGLE_MIN <= item['adjusted_steering_angle']
+                          and item['section_type'] != 'straight']
+        none_corner = [item for item in road_section if
+                            WEEK_CORNER_ANGLE_MIN > item['adjusted_steering_angle']
+                            and item['section_type'] != 'straight']
 
          # プロットの準備
         fig, ax = plt.subplots()
         gpd.GeoSeries(gdf_first.geometry).plot(ax=ax, color='gray')
-
+        week_sections = [item['points'] for item in week_corner]
+        medium_sections = [item['points'] for item in medium_corner]
+        strong_sections = [item['points'] for item in strong_corner]
+        none_sections = [item['points'] for item in none_corner]
         
-        medium_coords = list(dict.fromkeys(itertools.chain(*[item['points'] for item in medium_corner])))
-        strong_coords = list(dict.fromkeys(itertools.chain(*[item['points'] for item in strong_corner])))
-        
-        if week_corner:
-            week_coords = list(dict.fromkeys(itertools.chain(*[item['points'] for item in week_corner])))
-            ax.scatter(*zip(*week_coords), color='blue', label='week', s=10)
-        if medium_corner:
-            medium_coords = list(dict.fromkeys(itertools.chain(*[item['points'] for item in medium_corner])))
-            ax.scatter(*zip(*medium_coords), color='green', label='medium', s=10)
-        if strong_corner:
-            strong_coords = list(dict.fromkeys(itertools.chain(*[item['points'] for item in strong_corner])))
-            ax.scatter(*zip(*strong_coords), color='red', label='strong', s=10)
+        if week_sections:
+            ax.plot([], [], color="blue", label='week')
+            for section in week_sections:
+                ax.plot(*zip(*section), color="blue", linewidth=2)
+        if medium_sections:
+            ax.plot([], [], color="green", label='medium')
+            for section in medium_sections:
+                ax.plot(*zip(*section), color="green", linewidth=2)
+        if strong_sections:
+            ax.plot([], [], color="red", label='strong')
+            for section in strong_sections:
+                ax.plot(*zip(*section), color="red", linewidth=2)
+        if none_sections:
+            ax.plot([], [], color="orange", label='none')
+            for section in none_sections:
+                ax.plot(*zip(*section), color="orange", linewidth=2)
         
         week_corner_distance = sum(
-            item['distance'] for item in corners
+            item['distance'] for item in road_section
             if (WEEK_CORNER_ANGLE_MIN <= item['adjusted_steering_angle'] < WEEK_CORNER_ANGLE_MAX)
+            and item['section_type'] != 'straight'
         )
         medium_corner_distance = sum(
-            item['distance'] for item in corners
+            item['distance'] for item in road_section
             if (MEDIUM_CORNER_ANGLE_MIN <= item['adjusted_steering_angle'] < MEDIUM_CORNER_ANGLE_MAX)
+            and item['section_type'] != 'straight'
         )
         strong_corner_distance = sum(
-            item['distance'] for item in corners
+            item['distance'] for item in road_section
             if STRONG_CORNER_ANGLE_MIN <= item['adjusted_steering_angle']
+            and item['section_type'] != 'straight'
         )
-        print(f"week: {week_corner_distance}m, medium: {medium_corner_distance}m, strong: {strong_corner_distance}m")
+        none_corner_distance = sum(
+            item['distance'] for item in road_section
+            if WEEK_CORNER_ANGLE_MIN > item['adjusted_steering_angle'] < WEEK_CORNER_ANGLE_MAX
+            and item['section_type'] != 'straight'
+        )
+        straight_distance = sum(
+            item['distance'] for item in road_section
+            if item['section_type'] == 'straight'
+        )
+        print(f"length: {gdf_first.length}")
+        print(f"week: {week_corner_distance}m, medium: {medium_corner_distance}m, strong: {strong_corner_distance}m, none: {none_corner_distance}m, straight: {straight_distance}m")
         print(f"all: {gdf_first.length}m")
 
 
