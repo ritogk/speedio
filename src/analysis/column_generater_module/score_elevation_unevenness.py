733fbd0de03cbf549474659dbc7b87bf98998a5b
ritogk <your.email@example.com>
2024-10-08 15:30:37 +0900

    プロミネンス調整

--- src/analysis/column_generater_module/score_elevation_unevenness.py
+++ src/analysis/column_generater_module/score_elevation_unevenness.py
@@ -6,15 +6,15 @@ from ...core.convert_range import convert_range
 def generate(gdf: GeoDataFrame) -> Series:
     def func(x):
         elevation_unevenness_count = x.elevation_unevenness_count
-        # 1-10の範囲を(0.3-1)に変換する。
-        max = 10
+        # 1-8の範囲を(0.4-1)に変換する。
+        max = 8
         if elevation_unevenness_count <= 0:
             return 0
         elif elevation_unevenness_count >= max:
             return 1
         
         old_min, old_max = 1, max
-        new_min, new_max = 0.3, 1
+        new_min, new_max = 0.4, 1
         converted_value = convert_range(elevation_unevenness_count, old_min, old_max, new_min, new_max)
         return converted_value
 
