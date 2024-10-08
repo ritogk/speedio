be278944b4fdab82ec4253484191a95a812f91c4
ritogk <your.email@example.com>
2024-10-08 19:13:18 +0900

    feat: ストレートの扱いを変更

--- run.py
+++ run.py
@@ -26,8 +26,7 @@ def run():
                           STRONG_CORNER_ANGLE_MIN <= item['adjusted_steering_angle']
                           and item['section_type'] != 'straight']
         none_corner = [item for item in road_section if
-                            WEEK_CORNER_ANGLE_MIN > item['adjusted_steering_angle']
-                            and item['section_type'] != 'straight']
+                            item['section_type'] == 'straight']
 
          # プロットの準備
         fig, ax = plt.subplots()
@@ -38,9 +37,9 @@ def run():
         none_sections = [item['points'] for item in none_corner]
         
         if week_sections:
-            ax.plot([], [], color="blue", label='week')
+            ax.plot([], [], color="orange", label='week')
             for section in week_sections:
-                ax.plot(*zip(*section), color="blue", linewidth=2)
+                ax.plot(*zip(*section), color="orange", linewidth=2)
         if medium_sections:
             ax.plot([], [], color="green", label='medium')
             for section in medium_sections:
@@ -50,9 +49,9 @@ def run():
             for section in strong_sections:
                 ax.plot(*zip(*section), color="red", linewidth=2)
         if none_sections:
-            ax.plot([], [], color="orange", label='none')
+            ax.plot([], [], color="lightgray", label='none')
             for section in none_sections:
-                ax.plot(*zip(*section), color="orange", linewidth=2)
+                ax.plot(*zip(*section), color="lightgray", linewidth=1)
         
         week_corner_distance = sum(
             item['distance'] for item in road_section
@@ -71,16 +70,15 @@ def run():
         )
         none_corner_distance = sum(
             item['distance'] for item in road_section
-            if WEEK_CORNER_ANGLE_MIN > item['adjusted_steering_angle'] < WEEK_CORNER_ANGLE_MAX
-            and item['section_type'] != 'straight'
-        )
-        straight_distance = sum(
-            item['distance'] for item in road_section
             if item['section_type'] == 'straight'
         )
-        print(f"length: {gdf_first.length}")
-        print(f"week: {week_corner_distance}m, medium: {medium_corner_distance}m, strong: {strong_corner_distance}m, none: {none_corner_distance}m, straight: {straight_distance}m")
-        print(f"all: {gdf_first.length}m")
+        # print(f"length: {gdf_first.length}")
+        # print(f"week: {week_corner_distance}m, medium: {medium_corner_distance}m, strong: {strong_corner_distance}m, none: {none_corner_distance}m")
+        # print(f"all: {gdf_first.length}m")
+
+        # for item in road_section:
+        #     if item['section_type'] == 'straight':
+        #         print(f"distance: {item['distance']}m, type: {item['section_type']}, angle: {item['adjusted_steering_angle']}")
 
 
         plt.legend()
