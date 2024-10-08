3e3678b3a6c0f5e4381bef2da423f074195e97df
ritogk <your.email@example.com>
2024-10-08 20:25:54 +0900

    feat: 標高の区間リストの平準化

--- src/analysis/column_generater_module/elevation_segment_list.py
+++ src/analysis/column_generater_module/elevation_segment_list.py
@@ -1,9 +1,12 @@
 from geopandas import GeoDataFrame
 from pandas import Series
 from tqdm import tqdm
+import pandas as pd
 from .core.segmnet import generate_segment_original_index_list
+from .core.smoother import generate_moving_average
 
 INTERVAL = 50
+WINDOW_SIZE = 5
 
 # 指定距離単位の標高一覧を生成する
 def generate(gdf: GeoDataFrame) -> Series:
@@ -13,8 +16,10 @@ def generate(gdf: GeoDataFrame) -> Series:
         elevation_segment_list = []
         for segment_index in segment_index_list:
             elevation_segment_list.append(x.elevation_smooth[segment_index])
-
-        return elevation_segment_list
+        # 移動平均で平準化
+        series = pd.Series(elevation_segment_list)
+        smooth_series = generate_moving_average(series, WINDOW_SIZE)
+        return smooth_series.tolist()
 
     tqdm.pandas()
     series = gdf.progress_apply(func, axis=1)
