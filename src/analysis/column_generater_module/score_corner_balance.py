0eb8e66da7c61ed9107f75a4e202ed93402e3e95
ritogk <your.email@example.com>
2024-10-09 16:32:00 +0900

    feat: コーナーのバランスを最適化

--- src/analysis/column_generater_module/score_corner_balance.py
+++ src/analysis/column_generater_module/score_corner_balance.py
@@ -2,22 +2,18 @@ from geopandas import GeoDataFrame
 from pandas import Series
 import numpy as np
 
-WEEK_WEIGHT = 1
-MEDIUM_WEIGHT = 1
-STRONG_WEIGHT = 1.15 # 強コーナーは印象が強いので重みで上げる
-NONE_WEIGHT = 1
-# 目視検証データから道幅を評価する
+# 各コーナーのバランスを評価する
 def generate(gdf: GeoDataFrame) -> Series:
     def func(row):
-        score_corner_week = row.score_corner_week * WEEK_WEIGHT
-        score_corner_medium = row.score_corner_medium * MEDIUM_WEIGHT
-        score_corner_strong = row.score_corner_strong * STRONG_WEIGHT
-        score_corner_none = row.score_corner_none * NONE_WEIGHT
+        score_corner_week = row.score_corner_week
+        score_corner_medium = row.score_corner_medium
+        score_corner_strong = row.score_corner_strong
+        score_corner_none = row.score_corner_none
         
         values = [score_corner_week, score_corner_medium, score_corner_strong, score_corner_none]
 
         # 正規化定数 k を設定（必要に応じて調整）大きくするほど標準偏差の影響が小さくなる。
-        k = 0.5
+        k = 0.4
 
         # 標準偏差を計算
         std_dev = np.std(values)
