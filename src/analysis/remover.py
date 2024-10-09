3a70401d9a6ab0028724c8d5c94475afb1e13311
ritogk <your.email@example.com>
2024-10-09 12:06:19 +0900

    elevation_height_and_length_ratioを削除

--- src/analysis/remover.py
+++ src/analysis/remover.py
@@ -1,5 +1,4 @@
 from .remover_module import filter_edge
 from .remover_module import reverse_edge
 from .remover_module import score
-from .remover_module import elevation_min_height
 from .remover_module import remove_road_section_small_count
--- src/analysis/remover_module/elevation_min_height.py
+++
@@ -1,6 +0,0 @@
-from geopandas import GeoDataFrame
-
-
-# 標高の変化が少ないエッジを削除する
-def remove(gdf: GeoDataFrame) -> GeoDataFrame:
-    return gdf[gdf["elevation_height_and_length_ratio"] > 0.02]
