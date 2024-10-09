0e4bfac965e43d9558beedd8581317eb700725d7
ritogk <your.email@example.com>
2024-10-09 16:57:16 +0900

    feat: 距離の評価関数を最適化

--- src/analysis/column_generater_module/score_length.py
+++ src/analysis/column_generater_module/score_length.py
@@ -1,16 +1,18 @@
 from geopandas import GeoDataFrame
 from pandas import Series
-
+from ...core.convert_range import convert_range
 
 # 距離のスコアを求める
 # 距離をスコア含めないと短い距離で特徴量が詰まった道の評価が高くなってしまう。
 def generate(gdf: GeoDataFrame) -> Series:
-    def func(x):
-        min = 1000
-        max = 4000
-        if x > max:
-            return 1
-        return (x - min) / (max - min)
+    def func(length: int):
+        old_min, old_max = 3000, 8000
+        new_min, new_max = 0.5, 1
+        if length >= old_max:
+            score = 1
+        else:
+            score = convert_range(length, old_min, old_max, new_min, new_max)
+        return score
 
     series = gdf["length"].apply(func)
     return series
