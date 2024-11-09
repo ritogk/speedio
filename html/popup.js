8f4153fd693e37b3c153fb338388e8e586d74fa4
ritogk <your.email@example.com>
2024-11-09 12:58:08 +0900

    ポップアップを最適化

--- html/popup.js
+++ html/popup.js
@@ -1,131 +1,126 @@
 export const generateHtml = (x) => {
   return `<div style="padding: 10px; background:white;transform: scale(1.7); transform-origin: top left; box-sizing: border-box;">
-        <span style="font-weight:bold;">Score</span>
+        <span style="font-weight:bold;">スコア_内訳</span>
         <table style="width:100%;" border="1">
           <tr style="background:bisque;">
-              <td>score_normalization</td>
+              <td>スコア_合計_正規化</td>
               <td>${truncateToTwoDecimals(x.score_normalization)}</td>
           </tr>
           <tr style="background:bisque;">
-              <td>score</td>
+              <td>スコア_合計</td>
               <td>${truncateToTwoDecimals(x.score)}</td>
           </tr>
           <tr style="background: lightcyan;">
-              <td>score_elevation</td>
+              <td>スコア_標高</td>
               <td>${truncateToTwoDecimals(x.score_elevation)}</td>
           </tr>
           <tr style="background: lightcyan;">
-              <td>score_elevation_unevenness</td>
+              <td>スコア_標高_凹凸</td>
               <td>${truncateToTwoDecimals(x.score_elevation_unevenness)}</td>
           </tr>
           <tr>
-              <td>score_width</td>
+              <td>スコア_道幅</td>
               <td>${truncateToTwoDecimals(x.score_width)}</td>
           </tr>
           <tr>
-              <td>score_length</td>
+              <td>スコア_距離</td>
               <td>${truncateToTwoDecimals(x.score_length)}</td>
           </tr>
           <tr>
-              <td>score_building</td>
+              <td>スコア_周辺の建物の数</td>
               <td>${truncateToTwoDecimals(x.score_building)}</td>
           </tr>
           <tr>
-              <td>score_tunnel_outside</td>
+              <td>スコア_トンネル外の距離</td>
               <td>${truncateToTwoDecimals(x.score_tunnel_outside)}</td>
           </tr>
           <tr style="background: #ffe1fe">
-              <td>score_corner_week</td>
+              <td>スコア_コーナー_弱</td>
               <td>${truncateToTwoDecimals(x.score_corner_week)}</td>
           </tr>
           <tr style="background: #ffe1fe">
-              <td>score_corner_medium</td>
+              <td>スコア_コーナー_中</td>
               <td>${truncateToTwoDecimals(x.score_corner_medium)}</td>
           </tr>
           <tr style="background: #ffe1fe">
-              <td>score_corner_strong</td>
+              <td>スコア_コーナー_強</td>
               <td>${truncateToTwoDecimals(x.score_corner_strong)}</td>
           </tr>
           <tr style="background: #ffe1fe">
-              <td>score_corner_none</td>
+              <td>スコア_コーナー_なし(直線)</td>
               <td>${truncateToTwoDecimals(x.score_corner_none)}</td>
           </tr>
           <tr style="background: mistyrose">
-              <td>score_corner_balance</td>
+              <td>スコア_コーナー_バランス</td>
               <td>${truncateToTwoDecimals(x.score_corner_balance)}</td>
           </tr>
         </table>
-        <button id="buttonElevationGraph">elevation_graph</button>
-        <div id="graphArea" hidden>
-            <canvas id="graphElevationCanvas" width="1000" height="600" style="background-color: white;"></canvas>
-        </div><br>
 
-        <button id="button3D" class="large-button">3d_normal</button><br>
-        <button id="button3dDriverView" class="large-button">3d_driver_view</button><br>
-
-        <span style="font-weight:bold;">Created</span>
+        <span style="font-weight:bold;">スコアを計算した各要素</span>
         <table style="width:100%;" border="1">
           <tr>
-              <td>building_nearby_cnt</td>
+              <td>周辺の建物の数</td>
               <td>${truncateToTwoDecimals(x.building_nearby_cnt)}</td>
           </tr>
           <tr>
-              <td>road_section_cnt</td>
+              <td>区間数</td>
               <td>${truncateToTwoDecimals(x.road_section_cnt)}</td>
           </tr>
           <tr>
-              <td>elevation_height</td>
+              <td>標高の高低差</td>
               <td>${truncateToTwoDecimals(x.elevation_height)}</td>
           </tr>
           <tr>
-              <td>elevation_unevenness_count</td>
+              <td>標高の凹凸数</td>
               <td>${truncateToTwoDecimals(x.elevation_unevenness_count)}</td>
           </tr>
           <tr>
-              <td>angle_deltas</td>
-              <td>${truncateToTwoDecimals(x.angle_deltas)}</td>
-          </tr>
-          <tr>
-              <td>steering_wheel_max_angle</td>
+              <td>ハンドルの最大切れ角</td>
               <td>${truncateToTwoDecimals(x.steering_wheel_max_angle)}</td>
           </tr>
           <tr>
-              <td>steering_wheel_avg_angle</td>
+              <td>ハンドルの平均切れ角</td>
               <td>${truncateToTwoDecimals(x.steering_wheel_avg_angle)}</td>
           </tr>
           <tr>
-              <td>tunnel_length</td>
+              <td>トンネルの区間距離</td>
               <td>${truncateToTwoDecimals(x.tunnel_length)}</td>
+          </tr>
+          <tr>
+              <td>距離</td>
+              <td>${truncateToTwoDecimals(x.length)}</td>
+          </tr>
+          <tr>
+              <td>トンネル(あり,なし)</td>
+              <td>${x.tunnel}</td>
+          </tr>
+          <tr>
+              <td>橋(あり,なし)</td>
+              <td>${x.bridge}</td>
           </tr>
         </table>
 
+        <button class="large-button" onclick="window.open('${
+          x.google_map_url
+        }', '_blank')"
+        >GoogleMapで表示</button><br>
+        
+        <button id="button3D" class="large-button">3Dで表示</button><br>
+
+        <button id="button3dDriverView" class="large-button">3Dで表示(運転者目線)</button><br>
+
+        <button id="buttonElevationGraph" class="large-button">標高グラフを表示</button>
+        <div id="graphArea" hidden>
+            <canvas id="graphElevationCanvas" width="1000" height="600" style="background-color: white;"></canvas>
+        </div><br>
         
         <span style="font-weight:bold;">Original</span>
         <table style="width:100%;" border="1">
           <tr>
-              <td>name</td>
+              <td>名称</td>
               <td>${x.name}</td>
           </tr>
-          <tr>
-              <td>length</td>
-              <td>${truncateToTwoDecimals(x.length)}</td>
-          </tr>
-          <tr>
-              <td>lanes</td>
-              <td>${x.lanes}</td>
-          </tr>
-          <tr>
-              <td>highway</td>
-              <td>${x.highway}</td>
-          </tr>
-          <tr>
-              <td>tunnel</td>
-              <td>${x.tunnel}</td>
-          </tr>
-          <tr>
-              <td>bridge</td>
-              <td>${x.bridge}</td>
-          </tr>
+          
         </table>
 
 
