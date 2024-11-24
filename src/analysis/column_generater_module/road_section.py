540d9f68d97a09bdb20ad2f889842605dacfb9f4
ritogk <your.email@example.com>
2024-11-24 12:30:16 +0900

    refactor

--- src/analysis/column_generater_module/road_section.py
+++ src/analysis/column_generater_module/road_section.py
@@ -12,86 +12,48 @@ STRAIGHT_ANGLE = 7
 #
 def generate(gdf: GeoDataFrame) -> Series:
     def func(row):
-        # ステアリングの方向が変わる毎にグループ化
-        target = row['steering_wheel_angle_info']
-        old_direction = target[0]['direction']
-        corners = []
-        # 右左コーナーの情報
-        corner = [target[0]]
-        for i in range(1, len(target)):
-            current_segment = target[i]
-            angle = current_segment['steering_angle']
-            distance = current_segment['distance']
-            direction = current_segment['direction']
-            if angle < STRAIGHT_ANGLE:
-                direction = 'straight'
-            
-            if direction == old_direction:
-                corner.append(current_segment)
-            else:
-                corners.append({'type': old_direction, 'steering_angle_info': corner})
-                corner = [current_segment]
-                old_direction = direction
-        # 最後の未処理のセグメントを追加
-        if len(corner) > 0:
-            corners.append({'type': old_direction, 'steering_angle_info': corner})
+        # 連続する左コーナー、右コーナー、ストレートをグループ化してセクションとする
+        sections = group_continuous_section(row['steering_wheel_angle_info'])
+        
+        # ストレート区間が100m未満の場合は半分に分割して前後のセクションと結合する ※1
+        sections = merge_min_straight_section(sections)
 
-        # ストレート区間が100m未満の場合は半分に分割して前後のコーナーと結合する
-        corners = merge_min_straight_section(corners)
-
-        # # 連続する同一方向のコーナーをマージ
-        corners = merge_continuous_corner_section(corners)
+        # # 連続する同一方向のセクションをマージ(※1で結合した場合に同一方向のコーナーが連続する場合があるため)
+        sections = merge_continuous_section_section(sections)
 
         # データを整形
         datas = []
-        for corner in corners:
-            steering_angle_info = corner['steering_angle_info']
-            max_steering_angle = max(steering_angle_info, key=lambda x: x['steering_angle'])['steering_angle']
-            avg_steering_angle = sum([x['steering_angle'] for x in steering_angle_info]) / len(steering_angle_info)
-            # コーナー内の座標をつなげる。
+        for section in sections:
+            steering_angle_info = section['steering_angle_info']
+            # セクション内の座標を1つにまとめる
             points = []
             for x in steering_angle_info:
                 points.append(x['start'])
                 points.append(x['center'])
                 points.append(x['end'])
-            # 並び順を維持したまま重複を削除
             points = list(dict.fromkeys(points))
-            # pointsから距離(m)を計算
+
+            # セクションの距離を計算
             distance = 0
             for i in range(len(points) - 1):
                 distance += geodesic(reversed(points[i]), reversed(points[i+1])).meters
             
-            # 標高の変化量を計算する。始点と終点を間の値を使っているためindex番号をずらす。
-            point_st = points[1]
-            point_end = points[-2]
-            coords = list(row.geometry.coords)
-            
-            index_st = coords.index(point_st)
-            index_end = coords.index(point_end)
-            elevation_corner = row.elevation[index_st:index_end+1]
-            # 始点と終点の間の標高値を追加
-            elevation_corner.insert(0, (row.elevation[index_st - 1] + row.elevation[index_st]) / 2)
-            elevation_corner.append((row.elevation[index_end + 1] + row.elevation[index_end]) / 2)
-            
-            # 標高の高さを取得
-            elevation_min = min(elevation_corner)
-            elevation_max = max(elevation_corner)
-            elevation_height = elevation_max - elevation_min
+            # ステアリング角度の最大値、平均値を取得
+            max_steering_angle = max(steering_angle_info, key=lambda x: x['steering_angle'])['steering_angle']
+            avg_steering_angle = sum([x['steering_angle'] for x in steering_angle_info]) / len(steering_angle_info)
 
-            # 勾配におうじてステアリング角度を調整
-            elevation_height_and_distance_ratio = elevation_height / distance
-            adjusted_steering_angle = max_steering_angle * scale_to_range(elevation_height_and_distance_ratio)
-
+            # ステアリング角度を調整する
+            adjusted_steering_angle, elevation_height = adjust_steering_angle(max_steering_angle, points, row.elevation, distance, row.geometry.coords)
             
             datas.append({
                 'max_steering_angle': max_steering_angle,
                 'avg_steering_angle': avg_steering_angle,
                 'adjusted_steering_angle': adjusted_steering_angle,
                 'elevation_height_and_distance_ratio': elevation_height / distance,
-                'section_type': corner['type'],
+                'section_type': section['type'],
                 'steering_direction': steering_angle_info[0]['direction'],
                 'points': points,
-                'corner_info': steering_angle_info,
+                'section_info': steering_angle_info,
                 'distance': distance,
                 'elevation_height': elevation_height,
             })
@@ -128,6 +90,33 @@ def generate(gdf: GeoDataFrame) -> Series:
 
     return series
 
+# 連続する左コーナー、右コーナー、ストレートをグループ化
+def group_continuous_section(target):
+    # ステアリングの方向が変わる毎にグループ化
+    old_direction = target[0]['direction']
+    sections = []
+    # 右左コーナーの情報
+    section_work = [target[0]]
+    for i in range(1, len(target)):
+        current_segment = target[i]
+        angle = current_segment['steering_angle']
+        distance = current_segment['distance']
+        direction = current_segment['direction']
+        if angle < STRAIGHT_ANGLE:
+            direction = 'straight'
+        
+        if direction == old_direction:
+            section_work.append(current_segment)
+        else:
+            sections.append({'type': old_direction, 'steering_angle_info': section_work})
+            section_work = [current_segment]
+            old_direction = direction
+    # 最後の未処理のセグメントを追加
+    if len(section_work) > 0:
+        sections.append({'type': old_direction, 'steering_angle_info': section_work})
+    return sections
+
+
 # 線形変換: 0.05~0.125 を 1~1.25 の範囲に変換
 def scale_to_range(val):
     if val <= 0.05:
@@ -138,13 +127,13 @@ def scale_to_range(val):
         return 1 + (val - 0.05) * (1.25 - 1) / (0.11 - 0.05)
 
 # 短いストレート区間をコーナーセクションにマージ
-def merge_min_straight_section(corners):
+def merge_min_straight_section(sections):
     # ストレート区間が100m未満の場合は半分に分割して前後のコーナーと結合する
-    adjusted_corners = copy.deepcopy(corners)
+    adjusted_sections = copy.deepcopy(sections)
     is_not_exists_min_straight = True
     while is_not_exists_min_straight:
         # ストレートを抽出
-        straights = [x for x in adjusted_corners if x['type'] == 'straight']
+        straights = [x for x in adjusted_sections if x['type'] == 'straight']
         # 100m以下のストレートを抽出
         min_straights = [x for x in straights if sum([y['distance'] for y in x['steering_angle_info']]) < STRAIGHT_DISTANCE]
         if len(min_straights) == 0:
@@ -152,43 +141,44 @@ def merge_min_straight_section(corners):
             break
         min_straight_first = min_straights[0]
         # 100m未満の先頭indexを取得
-        min_straight_first_index = adjusted_corners.index(min_straight_first)
+        min_straight_first_index = adjusted_sections.index(min_straight_first)
         steering_angle_info = min_straight_first['steering_angle_info']
         # 前後のコーナーを取得
         if min_straight_first_index == 0:
             # 先頭のコーナーの処理
-            next_corner = copy.deepcopy(adjusted_corners[min_straight_first_index + 1])
-            next_corner['steering_angle_info'] = steering_angle_info + next_corner['steering_angle_info']
-            adjusted_corners[min_straight_first_index + 1] = next_corner
-        elif min_straight_first_index == len(adjusted_corners) - 1:
+            next_section = copy.deepcopy(adjusted_sections[min_straight_first_index + 1])
+            next_section['steering_angle_info'] = steering_angle_info + next_section['steering_angle_info']
+            adjusted_sections[min_straight_first_index + 1] = next_section
+        elif min_straight_first_index == len(adjusted_sections) - 1:
             # 末尾のコーナーの処理
-            previous_corner = copy.deepcopy(adjusted_corners[min_straight_first_index - 1])
-            previous_corner['steering_angle_info'] += steering_angle_info
-            adjusted_corners[min_straight_first_index - 1] = previous_corner
+            previous_section = copy.deepcopy(adjusted_sections[min_straight_first_index - 1])
+            previous_section['steering_angle_info'] += steering_angle_info
+            adjusted_sections[min_straight_first_index - 1] = previous_section
         else:
             if(len(steering_angle_info)) >= 2:
                 # 前後のコーナーにマージ
-                previous_corner = copy.deepcopy(adjusted_corners[min_straight_first_index - 1])
-                next_corner = copy.deepcopy(adjusted_corners[min_straight_first_index + 1])
+                previous_section = copy.deepcopy(adjusted_sections[min_straight_first_index - 1])
+                next_section = copy.deepcopy(adjusted_sections[min_straight_first_index + 1])
                 # 厳密にストレートの距離の半分を分割するべきだが、一旦は要素数から分割
-                previous_corner['steering_angle_info'] += steering_angle_info[:len(steering_angle_info)//2]
-                next_corner['steering_angle_info'] = steering_angle_info[len(steering_angle_info)//2:] + next_corner['steering_angle_info']
+                previous_section['steering_angle_info'] += steering_angle_info[:len(steering_angle_info)//2]
+                next_section['steering_angle_info'] = steering_angle_info[len(steering_angle_info)//2:] + next_section['steering_angle_info']
 
-                adjusted_corners[min_straight_first_index - 1] = previous_corner
-                adjusted_corners[min_straight_first_index + 1] = next_corner
+                adjusted_sections[min_straight_first_index - 1] = previous_section
+                adjusted_sections[min_straight_first_index + 1] = next_section
             else:
                 # 1件だけの場合は直前のコーナーにマージ
-                previous_corner = copy.deepcopy(adjusted_corners[min_straight_first_index - 1])
-                previous_corner['steering_angle_info'] += steering_angle_info
-                adjusted_corners[min_straight_first_index - 1] = previous_corner
+                previous_section = copy.deepcopy(adjusted_sections[min_straight_first_index - 1])
+                previous_section['steering_angle_info'] += steering_angle_info
+                adjusted_sections[min_straight_first_index - 1] = previous_section
 
         # ストレートを削除
-        adjusted_corners.remove(min_straight_first)
-    return adjusted_corners
+        adjusted_sections.remove(min_straight_first)
+    return adjusted_sections
+    
 
 # 連続する同一方向のコーナーをマージ
 # 同じ方向のコーナーが連続する場合は結合する。
-def merge_continuous_corner_section(road_sections):
+def merge_continuous_section_section(road_sections):
     merged_lst = []
     i = 0
     target_sections = road_sections
@@ -204,3 +194,29 @@ def merge_continuous_corner_section(road_sections):
         merged_lst.append(current_section)
         i += 1  # 次のセクションに移動
     return merged_lst
+
+# ステアリング角度を調整する
+def adjust_steering_angle(steering_angle, points, elevation, distance, coords):
+    # 標高の変化量を計算する。始点と終点を間の値を使っているためindex番号をずらす。
+    point_st = points[1]
+    point_end = points[-2]
+    coords = list(coords)
+
+    # row.geometry.coords
+    
+    index_st = coords.index(point_st)
+    index_end = coords.index(point_end)
+    elevation_section = elevation[index_st:index_end+1]
+    # 始点と終点の間の標高値を追加(ステアリング切れ角の座標は中間点なのでそれを補完するためのもの)
+    elevation_section.insert(0, (elevation[index_st - 1] + elevation[index_st]) / 2)
+    elevation_section.append((elevation[index_end + 1] + elevation[index_end]) / 2)
+    
+    # 標高の高さを取得
+    elevation_min = min(elevation_section)
+    elevation_max = max(elevation_section)
+    elevation_height = elevation_max - elevation_min
+
+    # 勾配がついている場合はステアリング角度を増やす
+    elevation_height_and_distance_ratio = elevation_height / distance
+    adjusted_steering_angle = steering_angle * scale_to_range(elevation_height_and_distance_ratio)
+    return adjusted_steering_angle, elevation_height
