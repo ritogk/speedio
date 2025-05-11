2ff7ad1defecfe0b44719bd8bc93b7dacd4c39e0
ritogk <your.email@example.com>
2025-05-11 15:27:22 +0900

    lp first commit

---
+++ product/lp/README.md
@@ -0,0 +1,13 @@
+# readme
+
+## 開発サーバー起動
+
+```
+npm run dev
+```
+
+## ssg
+
+```
+npm run generate
+```
---
+++ product/lp/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>tailwindcss sample</h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <router-link to="/about">Aboutへ</router-link>
+  </div>
+</template>
